48372294eb5483d20048f4de69b59d7238b72194
ratlaw <ratlaw@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2007-01-17 19:50:28 +0000

    Updated and working

--- testing/unittest/index.php
+++ testing/unittest/index.php
@@ -4,8 +4,13 @@ define( 'JPATH_BASE'   , dirname( dirname(__FILE__) ) );
 
 require_once( JPATH_BASE.'/includes/defines.php' );
 require_once( JPATH_BASE.'/libraries/loader.php' );
-require_once( JPATH_BASE.'/libraries/joomla/common/abstract/object.php' ); // Hmm!!!!
 require_once( JPATH_BASE.'/unittest/UnitTestController.php' );
+
+/**
+ * TODO: Check why loader.php needed require_once?
+ * TODO: Confirm if we can move these to a general location, or think about the use of extends?
+ */
+require_once( JPATH_BASE.'/libraries/joomla/common/abstract/object.php' ); // JObject
 
 // If a path is provided run the test
 if( $path = @$_REQUEST[ 'path' ] )
@@ -32,7 +37,13 @@ if( $path = @$_REQUEST[ 'path' ] )
 // Show the default start page
 else
 {
+    $testCases = array();
     $tests = UnitTestController::getUnitTestsList();
+    
+    foreach( $tests as $path )
+        if( count( explode( DIRECTORY_SEPARATOR, $path ) ) > 1 )
+            $testCases[$path] = implode( '->', array_slice( explode( DIRECTORY_SEPARATOR, $path ), 1 ) );
+    
     include( JPATH_BASE.'/unittest/views/default.html' );
 }
 ?>
