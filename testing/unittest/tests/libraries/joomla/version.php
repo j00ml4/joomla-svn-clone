305350df616a49d7cb4c1a6181ce11d24e8c4a32
ratlaw <ratlaw@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2007-01-19 00:33:33 +0000

    Completed test case for JVersion.php

--- testing/unittest/tests/libraries/joomla/version.php
+++ testing/unittest/tests/libraries/joomla/version.php
@@ -1,24 +1,40 @@
 <?php
 class TestJVersion extends UnitTestCase
 {
+    var $class = null;
+    
+    function TestJVersion()
+    {
+        $this->class = new JVersion();
+    }
+    
     function testGetLongVersion()
     {
-        $this->assertTrue( true );
+        $version = $this->class->PRODUCT.' '.
+            $this->class->RELEASE.'.'.
+            $this->class->DEV_LEVEL.' '.
+            $this->class->DEV_STATUS.
+            ' [ '.$this->class->CODENAME.' ] '.
+            $this->class->RELDATE.' '.
+            $this->class->RELTIME.' '.
+            $this->class->RELTZ;
+        
+        $this->assertTrue( $this->class->getLongVersion() == $version );
     }
 
     function testGetShortVersion()
     {
-        $this->assertTrue( true );
+        $this->assertTrue( $this->class->getShortVersion() == $this->class->RELEASE.'.'.$this->class->DEV_LEVEL );
     }
 
     function testGetHelpVersion()
     {
-        $this->assertTrue( true );
+        $this->assertTrue( $this->class->getHelpVersion() == '.'.str_replace( '.', '', $this->class->RELEASE ) );
     }
 
     function testIsCompatible()
     {
-        $this->assertTrue( true );
+        $this->assertTrue( $this->class->isCompatible ( $this->class->RELEASE.'.'.$this->class->DEV_LEVEL ) );
     }
 
 }
