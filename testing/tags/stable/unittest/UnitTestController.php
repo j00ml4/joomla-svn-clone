f61900ce3d9b3c453e3868338f0f4242b5f7a4bc
instance <instance@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2007-02-27 22:31:46 +0000

    This tag will track the latest stable version of the test framework.

---
+++ testing/tags/stable/unittest/UnitTestController.php
@@ -0,0 +1,154 @@
+<?php
+require_once( dirname(__FILE__).'/simpletest/unit_tester.php' );
+require_once( dirname(__FILE__).'/simpletest/reporter.php' );
+
+class UnitTestController extends GroupTest
+{
+    function UnitTestController( $path, &$reporter, $label='' )
+    {
+        $this->GroupTest( $label );
+        
+        $files = array();
+        
+        if( is_dir( $path ) )
+            $files = $this->_files( $path );
+        elseif( is_file( $path ) )
+            $files[] = $path;
+        
+        // Make new test files
+        if( count( $files ) == 0 )
+        {
+            $this->makeUnitTestFiles( str_replace( UNITTEST_BASE.DIRECTORY_SEPARATOR, '..'.DIRECTORY_SEPARATOR, $path ) );
+            return $this->UnitTestController( $path, $reporter, $label );
+        }
+            
+        foreach( $files as $file )
+        {
+            include_once( str_replace( UNITTEST_BASE.DIRECTORY_SEPARATOR, '..'.DIRECTORY_SEPARATOR, $file ) );
+            $this->addTestFile( $file );
+        }
+        
+        $this->run( $reporter );
+    }
+    
+    function makeUnitTestFiles( $path )
+    {
+        $files = array();
+        
+        if( is_dir( $path ) )
+            $files = $this->_files( $path );
+        elseif( is_file( $path ) )
+            $files[] = $path;
+        
+        foreach( $files as $file ) $this->makeUnitTestFile( $file );
+    }
+    
+    function makeUnitTestFile( $sourceFile )
+    {
+        $file = UNITTEST_ROOT.DIRECTORY_SEPARATOR.UNITTEST_BASE . str_replace( '..', '', $sourceFile ); // create path in test dir
+        
+        // Check if the file is already in the test dir, if it is return
+        if( file_exists( $file ) ) return;
+        
+        // TODO: legacy  will need to be done by hand
+        if( strpos( $sourceFile, 'legacy' ) ) return;
+        // TODO: patTemplate will need to be done by hand
+        if( strpos( $sourceFile, 'template' ) ) return;
+        
+        $dir = dirname( $file );
+        if( !is_dir( $dir ) ) mkdir( $dir, 0777, true ); // Create the dir if not found
+        
+        include_once( $sourceFile );
+        $class = $this->_getClassFromFile( $sourceFile );
+        
+        $handle = fopen( $file, 'wb' );
+        fwrite( $handle, $this->getUnitTestTemplate( $class, get_class_methods( $class ) ) );
+        fclose( $handle );
+    }
+    
+    function getUnitTestTemplate( $class, $methods )
+    {
+        if( !is_array( $methods ) ) return;
+        
+        // remove any overloaded methods
+        $methods = array_unique( $methods );
+        
+        $line = "<?php\n";
+        $line.= "class TestOf{$class} extends UnitTestCase\n";
+        $line.= "{\n";
+        foreach( $methods as $method )
+        {
+            $line.= "    function test".ucfirst($method)."()\n";
+            $line.= "    {\n";
+            $line.= "        \$this->assertTrue( false );\n";
+            $line.= "    }\n\n";
+        }
+        $line.= "}\n";
+        $line.= "?>";
+        
+        return $line;
+    }
+    
+    function getUnitTestsList()
+    {
+        $tests = UnitTestController::_files( UNITTEST_BASE, true );
+        
+        return array_merge( array( UNITTEST_BASE ), $tests );
+    }
+    
+    function _files( $path, $showFolders=false )
+	{
+		// Initialize variables
+		$files = array ();
+        
+		// Is the path a folder?
+		if ( !is_dir( $path ) ) return $files;
+
+		// read the source directory
+		$handle = opendir( $path );
+		
+		// First get Files fron the Dir
+		while ( $file = readdir( $handle ) )
+		{
+			$fullpath = $path.DIRECTORY_SEPARATOR.$file;
+
+			if ( is_file( $fullpath ) && substr( $file, -4 ) == '.php' )
+			{
+				$files[] = $fullpath;
+			}
+		}
+		
+		rewinddir( $handle );
+		
+		// Now check directories in the dir
+	    while ( $file = readdir( $handle ) )
+		{
+			$fullpath = $path.DIRECTORY_SEPARATOR.$file;
+
+			if ( is_dir( $fullpath ) && $file != '.' && $file != '..' && $file != '.svn' )
+			{
+			    if( $showFolders ) $files[] = $fullpath;
+			    
+				$folder = UnitTestController::_files( $fullpath, $showFolders );
+				$files = array_merge( $files, $folder );
+			}
+		}
+		
+		closedir( $handle );
+		
+		return $files;
+	}
+	
+	function _getClassFromFile( $path )
+	{
+	    $lines = file( $path );
+        
+        foreach( $lines as $line )
+            if( strtolower( substr( $line, 0, 5 ) ) == 'class' )
+            {
+                $parts = explode( ' ', $line );
+                return trim( @$parts[1] );
+            }
+	}
+}
+?>
---
+++ testing/tags/stable/unittest/views/JoomlaHtml.php
@@ -0,0 +1,146 @@
+<?php
+class JoomlaHtml extends SimpleReporter
+{
+    /**
+     *    Paints the top of the web page setting the
+     *    title to the name of the starting test.
+     *    @param string $test_name      Name class of test.
+     *    @access public
+     */
+    function paintHeader( $test_name )
+    {
+        header( "Expires: Mon, 26 Jul 1997 05:00:00 GMT" );
+        header( "Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT" );
+        header( "Cache-Control: no-store, no-cache, must-revalidate" );
+        header( "Cache-Control: post-check=0, pre-check=0", false );
+        header( "Pragma: no-cache" );
+        
+        flush();
+        
+        echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">";
+        echo "<html>\n<head>\n<title>Testcase: $test_name</title>\n";
+        echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n";
+        echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"views/style.css\" />\n";
+        echo "</head>\n<body>\n";
+        echo "<h1 class=\"header\">Testcase: $test_name</h1>\n";
+    }
+    
+    /**
+     *    Paints the end of the test with a summary of
+     *    the passes and failures.
+     *    @param string $test_name        Name class of test.
+     *    @access public
+     */
+    function paintFooter( $test_name )
+    {
+        $state = ($this->getFailCount() + $this->getExceptionCount() > 0 ? "fail" : "pass");
+        
+        echo "<div class=\"footer_{$state}\">";
+        echo $this->getTestCaseProgress() . "/" . $this->getTestCaseCount();
+        echo " test cases complete:\n";
+        echo "<strong>" . $this->getPassCount() . "</strong> passes, ";
+        echo "<strong>" . $this->getFailCount() . "</strong> fails and ";
+        echo "<strong>" . $this->getExceptionCount() . "</strong> exceptions.";
+        echo "</div>\n";
+        echo "<div class=\"footer_footer\"><a href=\"?\">&lt;= Home</a></div>\n";
+        echo "</body>\n</html>\n";
+    }
+    
+    function paintPass( $message )
+    {
+        parent::paintPass( $message );
+        
+        echo "<div class=\"pass\">";
+        
+        $breadcrumb = $this->getTestList();
+        array_shift( $breadcrumb );
+        
+        echo implode(" -&gt; ", $breadcrumb );
+        
+        echo " -&gt; " . $this->_htmlEntities( $message );
+        echo "</div>";
+    }
+    /**
+     *    Paints the test failure with a breadcrumbs
+     *    trail of the nesting test suites below the
+     *    top level test.
+     *    @param string $message    Failure message displayed in
+     *                              the context of the other tests.
+     *    @access public
+     */
+    function paintFail( $message )
+    {
+        parent::paintFail( $message );
+        
+        echo "<div class=\"fail\">";
+        
+        $breadcrumb = $this->getTestList();
+        array_shift( $breadcrumb );
+        
+        echo implode(" -&gt; ", $breadcrumb );
+        
+        echo " -&gt; " . $this->_htmlEntities( $message );
+        echo "</div>";
+    }
+    
+    /**
+     *    Paints a PHP error or exception.
+     *    @param string $message        Message is ignored.
+     *    @access public
+     *    @abstract
+     */
+    function paintException( $message )
+    {
+        parent::paintException( $message );
+        
+        echo "<div class=\"exception\">";
+        
+        $breadcrumb = $this->getTestList();
+        array_shift( $breadcrumb );
+        
+        echo implode( " -&gt; ", $breadcrumb );
+        echo " -&gt; " . $this->_htmlEntities($message);
+        echo "</div>";
+    }
+    
+    /**
+     *    Paints a simple supplementary message.
+     *    @param string $message        Text to display.
+     *    @access public
+     */
+    function paintMessage( $message )
+    {
+        parent::paintMessage( $message );
+        
+        echo "<div class=\"message\">";
+        
+        $breadcrumb = $this->getTestList();
+        array_shift( $breadcrumb );
+        
+        echo implode( " -&gt; ", $breadcrumb );
+        echo " -&gt; " . $this->_htmlEntities($message);
+        echo "</div>";
+    }
+    
+    /**
+     *    Paints formatted text such as dumped variables.
+     *    @param string $message        Text to show.
+     *    @access public
+     */
+    function paintFormattedMessage( $message )
+    {
+        echo '<pre class="message">'.$this->_htmlEntities( $message ).'</pre>';
+    }
+    
+    /**
+     *    Character set adjusted entity conversion.
+     *    @param string $message    Plain text or Unicode message.
+     *    @return string            Browser readable message.
+     *    @access protected
+     */
+    function _htmlEntities( $message )
+    {
+        return htmlentities( $message, ENT_COMPAT, 'UTF-8' );
+    }
+}
+?>
