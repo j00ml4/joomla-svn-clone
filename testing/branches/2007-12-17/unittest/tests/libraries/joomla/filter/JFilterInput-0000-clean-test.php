7d73a6e979e6426a533a549058de83839d48c124
tsai146 <tsai146@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-03-07 22:50:04 +0000

    Add JFilterInputTest_Clean test 0000

---
+++ testing/branches/2007-12-17/unittest/tests/libraries/joomla/filter/JFilterInput-0000-clean-test.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * JFilterInput clean tests
+ *
+ * @package Joomla
+ * @subpackage UnitTest
+ * @version $Id: $
+ * @author Jui-Yu Tsai <raytsai@gmail.com>
+ */
+
+// Call JFilterInputTest_Clean::main() if this source file is executed directly.
+if (! defined('JUNIT_MAIN_METHOD')) {
+	define('JUNIT_MAIN_METHOD', 'JFilterInputTest_Clean::main');
+	$JUnit_home = DIRECTORY_SEPARATOR . 'unittest' . DIRECTORY_SEPARATOR;
+	if (($JUnit_posn = strpos(__FILE__, $JUnit_home)) === false) {
+		die('Unable to find ' . $JUnit_home . ' in path.');
+	}
+	$JUnit_posn += strlen($JUnit_home) - 1;
+	$JUnit_root = substr(__FILE__, 0, $JUnit_posn);
+	$JUnit_start = substr(
+		__FILE__,
+		$JUnit_posn + 1,
+		strlen(__FILE__) - strlen(basename(__FILE__)) - $JUnit_posn - 2
+	);
+	require_once $JUnit_root . DIRECTORY_SEPARATOR . 'setup.php';
+}
+
+/*
+ * Now load the Joomla environment
+ */
+if (! defined('_JEXEC')) {
+	define('_JEXEC', 1);
+}
+require_once JPATH_BASE . '/includes/defines.php';
+/*
+ * Mock classes
+ */
+ 
+/*
+ * We now return to our regularly scheduled environment.
+ */
+require_once JPATH_LIBRARIES . '/joomla/import.php';
+
+jimport( 'joomla.filter.filterinput' );
+
+class JFilterInputTest_Clean extends PHPUnit_Framework_TestCase {
+	/**
+	 * Runs the test methods of this class.
+	 */
+	static function main() {
+		$suite  = new PHPUnit_Framework_TestSuite(__CLASS__);
+		$result = PHPUnit_TextUI_TestRunner::run($suite);
+	}
+
+	function setUp() {
+		
+	}
+	
+	static function dataSet() {
+	    $cases = array(
+			'int' => array(
+				'int',
+				'123abc',
+				'123'
+			),
+			'word' => array(
+				'word',
+				'ab3cd',
+				'abcd'
+			),
+		);
+		$tests = $cases;
+		
+		return $tests;
+	}
+
+    /**
+	 * Execute a clean test case.
+	 *
+	 * The test framework calls this function once for each element in the array
+	 * returned by the named data provider.
+	 *
+	 * @dataProvider dataSet
+	 * @param string The type of input
+	 * @param string The input 
+	 * @param string The expected result for this test.
+	 */
+	function testClean($type, $data, $expect) {
+		$this->assertEquals($expect, JFilterInput::clean($data, $type));
+	}
+
+}
+
+// Call JFilterInputTest::main() if this source file is executed directly.
+if (JUNIT_MAIN_METHOD == 'JFilterInputTest_Clean::main') {
+	JFilterInputTest_Clean::main();
+}
