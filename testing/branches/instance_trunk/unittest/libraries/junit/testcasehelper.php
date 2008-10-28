40f6072b739b2249b7721e2c2923ca27c39d7b49
instance <instance@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2008-10-28 20:07:26 +0000

    Helper class with string difference display function, since PHPUnit seems to give up in some cases.

---
+++ testing/branches/instance_trunk/unittest/libraries/junit/testcasehelper.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Joomla! Unit Test Facility.
+ *
+ * @package Joomla
+ * @subpackage UnitTest
+ * @copyright Copyright (C) 2005 - 2008 Open Source Matters, Inc.
+ * @version $Id: $
+ */
+
+/**
+ * Utility functions for test cases
+ */
+class JUnit_TestCaseHelper {
+
+    /**
+     * Dump information on where two strings differ.
+     */
+    static function stringDiff($actual, $expect) {
+        $scanTo = min(strlen($actual), strlen($expect));
+        for ($posn = 0; $posn < $scanTo; ++$posn) {
+            if ($actual[$posn] != $expect[$posn]) {
+                break;
+            }
+        }
+        if ($posn > 30) {
+            $from = $posn - 30;
+            $bias = 30;
+        } else {
+            $from = 0;
+            $bias = $posn;
+        }
+        $aSeg = substr($actual, $from, 60);
+        $eSeg = substr($expect, $from, 60);
+        // If the strings match, return nothing.
+        if ($aSeg == $eSeg) {
+            return '';
+        }
+        $scanTo = max(strlen($aSeg), strlen($eSeg));
+        $endAt = min(strlen($aSeg), strlen($eSeg));
+        $diff = str_repeat(' ', $scanTo);
+        for ($ind = 0; $ind < $scanTo; ++$ind) {
+            if ($ind < strlen($eSeg) && ($ech = $eSeg[$ind]) < ' ') {
+                $eSeg[$ind] = ' ';
+                $diff[$ind] = '-';
+            }
+            if ($ind < strlen($aSeg) && ($ach = $aSeg[$ind]) < ' ') {
+                $aSeg[$ind] = ' ';
+                $diff[$ind] = '-';
+            }
+            if ($ind >= $endAt) {
+                $diff[$ind] = '*';
+                continue;
+            }
+            if ($ech != $ach) {
+                $diff[$ind] = '|';
+            }
+        }
+        return 'Mismatched strings at ' . $posn . chr(10)
+            . 'Actual: <' . $aSeg . '>' . chr(10)
+            . 'Diff:    ' . $diff . chr(10)
+            . 'Expect: <' . $eSeg . '>' . chr(10);
+    }
+
+}
+
