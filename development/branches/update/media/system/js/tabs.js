f7fb1febf068f61918af4bb715ef86bced6392b8
pasamio <pasamio@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-05-30 13:50:42 +0000

    Reinstated update branch, copying from trunk

---
+++ development/branches/update/media/system/js/tabs.js
@@ -0,0 +1,66 @@
+/**
+ * @version		$Id$
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License <http://www.gnu.org/copyleft/gpl.html>
+ */
+
+/**
+ * Tabs behavior
+ *
+ * @package		Joomla!
+ * @subpackage	JavaScript
+ * @since		1.5
+ */
+var JTabs = new Class({
+
+    getOptions: function(){
+        return {
+
+            display: 0,
+
+            onActive: function(title, description){
+                description.setStyle('display', 'block');
+                title.addClass('open').removeClass('closed');
+            },
+
+            onBackground: function(title, description){
+                description.setStyle('display', 'none');
+                title.addClass('closed').removeClass('open');
+            }
+        };
+    },
+
+    initialize: function(dlist, options){
+        this.dlist = $(dlist);
+        this.setOptions(this.getOptions(), options);
+        this.titles = this.dlist.getElements('dt');
+        this.descriptions = this.dlist.getElements('dd');
+        this.content = new Element('div').injectAfter(this.dlist).addClass('current');
+
+        for (var i = 0, l = this.titles.length; i < l; i++){
+            var title = this.titles[i];
+            var description = this.descriptions[i];
+            title.setStyle('cursor', 'pointer');
+            title.addEvent('click', this.display.bind(this, i));
+            description.injectInside(this.content);
+        }
+
+        if ($chk(this.options.display)) this.display(this.options.display);
+
+        if (this.options.initialize) this.options.initialize.call(this);
+    },
+
+    hideAllBut: function(but){
+        for (var i = 0, l = this.titles.length; i < l; i++){
+            if (i != but) this.fireEvent('onBackground', [this.titles[i], this.descriptions[i]])
+        }
+    },
+
+    display: function(i){
+        this.hideAllBut(i);
+        this.fireEvent('onActive', [this.titles[i], this.descriptions[i]])
+    }
+});
+
+JTabs.implement(new Events);
+JTabs.implement(new Options);
