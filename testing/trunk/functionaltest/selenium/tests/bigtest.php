7c688fe7a8fa13c1e550e777bc53d2af86b71d97
dextercowley <dextercowley@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-08-12 04:25:09 +0000

    Updated tests to run either from Eclipse or command line

--- testing/trunk/functionaltest/selenium/tests/bigtest.php
+++ testing/trunk/functionaltest/selenium/tests/bigtest.php
@@ -1,31 +1,46 @@
 <?php
 /* com_content
- * Verifies that all the toolbar icons are present.
+ * 
+ * 
  */
 
 require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 
-class ComContent0002 extends PHPUnit_Extensions_SeleniumTestCase
+class BigTest extends PHPUnit_Extensions_SeleniumTestCase
 {
+
   function setUp()
   {
     $cfg = new SeleniumConfig();
-    $this->setBrowser("*firefox");
+    $this->setBrowser($cfg->browser);
     $this->setBrowserUrl($cfg->host.$cfg->path);
   }
 
   function testMyTestCase()
   {
-  	print("Running com_content0002.php" . "\n");
+  	print("Starting bigtest.php" . "\n");
     $cfg = new SeleniumConfig();
-
-    print("Log into back end." . "\n");
-    $this->open($cfg->path.'administrator/');
+  	print("Log into back end." . "\n");
+    
+    $this->open($cfg->path . "administrator");
+    $this->waitForPageToLoad("30000");
     $this->type("modlgn_username", $cfg->username);
     $this->type("modlgn_passwd", $cfg->password);
-    $this->click("//input[@value='Login']");
+    $this->click("link=Login");
     $this->waitForPageToLoad("30000");
-
+  	print("Load article manager." . "\n");
+    $this->click("link=Article Manager");
+    $this->waitForPageToLoad("30000");
+    print("Page down" . "\n");
+    $this->click("link=Next");
+    $this->waitForPageToLoad("30000");
+    $this->open($cfg->path . "administrator");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=Article Manager");
+    $this->waitForPageToLoad("30000");
+    $this->click("//div[@id='element-box']/div[2]/form/table[2]/tbody/tr[15]/td[5]/a/img");
+    $this->waitForPageToLoad("30000");
+    
     print("Check that Content links are present." . "\n");
     $this->assertTrue($this->isElementPresent("link=Article Manager"));
     $this->assertTrue($this->isElementPresent("link=Article Trash"));
@@ -49,6 +64,10 @@ class ComContent0002 extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertTrue($this->isElementPresent("//td[@id='toolbar-new']/a/span"));
     $this->assertTrue($this->isElementPresent("link=Parameters"));
     $this->assertTrue($this->isElementPresent("//td[@id='toolbar-help']/a/span"));
+    
+    print("Go to top of article list." . "\n");
+    $this->click("link=Start");
+    $this->waitForPageToLoad("30000");
 
     print("Load an article for editing." . "\n");
     $this->click("link=Example Pages and Menu Links");
@@ -175,9 +194,144 @@ class ComContent0002 extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertTrue($this->isElementPresent("//td[@id='toolbar-delete']/a/span"));
     $this->assertTrue($this->isElementPresent("//a[@onclick=\"popupWindow('http://help.joomla.org/index2.php?option=com_content&task=findkey&tmpl=component;1&keyref=screen.frontpage.15', 'Help', 640, 480, 1)\"]"));
   	print("Logout." . "\n");
+    
+        print("Navigate to front end" . "\n");
+    $this->open($cfg->path);
+    print("Select Joomla! Overview from left menu" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[1]/div/div/div/ul/li[2]/a/span");
+    $this->waitForPageToLoad("30000"); 
+    print("Check title, text, and buttons" . "\n");
+    $this->assertEquals("Joomla! Overview", $this->getTitle());
+    $this->assertEquals("Joomla! Overview", $this->getText("//div[@id='maincolumn']/table[2]/tbody/tr/td/table[1]/tbody/tr/td[1]"));
+    $this->assertEquals("To get the perfect Web site with all the functionality that you require for your particular application may take additional time and effort, but with the Joomla! Community support that is available and the many Third Party Developers actively creating and releasing new Extensions for the 1.5 platform on an almost daily basis, there is likely to be something out there to meet your needs. Or you could develop your own Extensions and make these available to the rest of the community.", $this->getText("//div[@id='maincolumn']/table[2]/tbody/tr/td/table[2]/tbody/tr[3]/td/p[7]"));
+    $this->assertTrue($this->isElementPresent("//img[@alt='PDF']"));
+    $this->assertTrue($this->isElementPresent("//img[@alt='Print']"));
+    $this->assertTrue($this->isElementPresent("//img[@alt='E-mail']"));
+    print("Navigate to home page" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[1]/div/div/div/ul/li[1]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Navigate to back end and log in" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[2]/div/div/div/ul/li[7]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Load Article Manager" . "\n");
+    $this->click("link=Article Manager");
+    $this->waitForPageToLoad("30000");
+    print("Open Joomla! Overview article for editing" . "\n");
+    $this->click("link=Joomla! Overview");
+    $this->waitForPageToLoad("30000");
+    print("Change show titles parameter to no" . "\n");
+    $this->click("//h3[@id='params-page']/span");
+    $this->select("paramsshow_title", "label=No");
+    print("Save the article" . "\n");
+    $this->click("//td[@id='toolbar-save']/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Navigate to front end" . "\n");
+    $this->open($cfg->path);
+    print("Navigate to Joomla! Overview" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[1]/div/div/div/ul/li[2]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Check that title does not show now" . "\n");
+    $this->assertNotEquals("Joomla! Overview", $this->getText("//div[@id='maincolumn']/table[2]/tbody/tr/td/table[1]/tbody/tr/td[1]"));
+    print("Navigate to home page" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[1]/div/div/div/ul/li[1]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Navigate to back end (still logged in)" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[2]/div/div/div/ul/li[7]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Navigate to Article Manager" . "\n");
+    $this->click("link=Article Manager");
+    $this->waitForPageToLoad("30000");
+    print("Edit Joomla! Overview article" . "\n");
+    $this->click("link=Joomla! Overview");
+    $this->waitForPageToLoad("30000");
+    print("Change parameter back to Use Global" . "\n");
+    $this->click("//h3[@id='params-page']/span");
+    $this->select("paramsshow_title", "label=Use Global");
+    print("Save article" . "\n");
+    $this->click("//td[@id='toolbar-save']/a/span");
+    $this->waitForPageToLoad("30000");
+  	
+  	print("Navigate to Joomla! Overview in front end" . "\n");
+    $this->open($cfg->path."index.php?option=com_content&view=article&id=19&Itemid=27");
+    print("Check that PDF icon shows" . "\n");
+    $this->assertTrue($this->isElementPresent("//img[@alt='PDF']"));
+    print("Navigate to home page in front end" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[1]/div/div/div/ul/li[1]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Navigate to back end and log in" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[2]/div/div/div/ul/li[7]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Load article manager" . "\n");
+    $this->click("link=Article Manager");
+    $this->waitForPageToLoad("30000");
+    print("Open Joomla! Overview for editing" . "\n");
+    $this->click("link=Joomla! Overview");
+    $this->waitForPageToLoad("30000");
+    print("Change parameter to hide PDF icon" . "\n");
+    $this->click("//h3[@id='params-page']/span");
+    $this->select("paramsshow_pdf_icon", "label=Hide");
+    print("Save article" . "\n");
+    $this->click("//td[@id='toolbar-save']/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Navigate to front end" . "\n");
+    $this->open($cfg->path);
+    print("Navigate to Joomla! Overview in front end" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[1]/div/div/div/ul/li[2]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Check that PDF icon does not show" . "\n");
+    $this->assertFalse($this->isElementPresent("//img[@alt='PDF']"));
+    print("Navigate to home page in front end" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[1]/div/div/div/ul/li[1]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Navigate to back end (still logged in)" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[2]/div/div/div/ul/li[7]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Navigate to Article Manager" . "\n");
+    $this->click("link=Article Manager");
+    $this->waitForPageToLoad("30000");
+    print("Edit Joomla! Overview" . "\n");
+    $this->click("link=Joomla! Overview");
+    $this->waitForPageToLoad("30000");
+    print("Change PDF parameter back to Use Global" . "\n");
+    $this->click("//h3[@id='params-page']/span");
+    $this->select("paramsshow_pdf_icon", "label=Use Global");
+    print("Save Joomla! Overview" . "\n");
+    $this->click("//td[@id='toolbar-save']/a/span");
+    $this->waitForPageToLoad("30000");
+    
+    $this->open($cfg->path."administrator/index.php");
+    $this->waitForPageToLoad("30000");
+    print("Open article manager" . "\n");
+    $this->click("link=Article Manager");
+    $this->waitForPageToLoad("30000");
+    print("Click on Unarchive icon in toolbar" . "\n");
+    $this->click("//td[@id='toolbar-unarchive']/a/span");
+    print("Check alert message" . "\n");
+    $this->assertEquals("Please select an Article from the list to unarchive", $this->getAlert());
+    print("Click archive icon in toolbar" . "\n");
+    $this->click("//td[@id='toolbar-archive']/a/span");
+    print("Check alert message" . "\n");
+    $this->assertEquals("Please select an Article from the list to archive", $this->getAlert());
+    print("Click publish icon in toolbar" . "\n");
+    $this->click("//td[@id='toolbar-publish']/a/span");
+    print("Check alert message" . "\n");
+    $this->assertEquals("Please select an Article from the list to publish", $this->getAlert());
+    print("Click other icons and check alert messages" . "\n");
+    $this->click("//td[@id='toolbar-unpublish']/a/span");
+    $this->assertEquals("Please select an Article from the list to unpublish", $this->getAlert());
+    $this->click("//td[@id='toolbar-move']/a/span");
+    $this->assertEquals("Please select an Article from the list to move", $this->getAlert());
+    $this->click("//td[@id='toolbar-copy']/a/span");
+    $this->assertEquals("Please select an Article from the list to copy", $this->getAlert());
+    $this->click("//td[@id='toolbar-trash']/a/span");
+    $this->assertEquals("Please select an Article from the list to trash", $this->getAlert());
+    $this->click("//td[@id='toolbar-edit']/a/span");
+    $this->assertEquals("Please select an Article from the list to edit", $this->getAlert());
+    
     $this->click("link=Logout");
     $this->waitForPageToLoad("30000");
-  	print("Finished com_content0002.php." . "\n");
+    print("Finished com_content0001.php." . "\n");
+    
   }
 }
 ?>
