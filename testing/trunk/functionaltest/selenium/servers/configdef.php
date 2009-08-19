9845c71453cfa0b111abf70dc3b5c484fb586e88
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-08-19 18:48:57 +0000

    Moving config file

--- testing/trunk/functionaltest/selenium/servers/configdef.php
+++ testing/trunk/functionaltest/selenium/servers/configdef.php
@@ -2,10 +2,9 @@
 
 class SeleniumConfig
 {
-	var $host = 'http://localhost';
-	var $path = '/joomla_development/Joomla_1.5.11_plus/';
+	var $host = 'http://10.0.0.104';
+	var $path = '/selsampledata/';
 	var $username = 'admin';
-	var $password = 'password';
+	var $password = 'admin';
 	var $browser = '*chrome';
-	var $baseURI = "c:/xampp/htdocs/joomla_development/Joomla_1.5.11_plus/";
 }
