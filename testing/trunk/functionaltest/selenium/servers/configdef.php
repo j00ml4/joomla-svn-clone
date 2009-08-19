9bb66665d38acabb316a9910a2d3826b72f60f84
dextercowley <dextercowley@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-08-19 00:53:26 +0000

    Add TinyMCE bug tests in 1515_tests folder and add fields to configdef.php to support these tests.

--- testing/trunk/functionaltest/selenium/servers/configdef.php
+++ testing/trunk/functionaltest/selenium/servers/configdef.php
@@ -3,8 +3,9 @@
 class SeleniumConfig
 {
 	var $host = 'http://localhost';
-	var $path = '/j1513test/';
+	var $path = '/joomla_development/Joomla_1.5.11_plus/';
 	var $username = 'admin';
 	var $password = 'password';
 	var $browser = '*chrome';
+	var $baseURI = "c:/xampp/htdocs/joomla_development/Joomla_1.5.11_plus/";
 }
