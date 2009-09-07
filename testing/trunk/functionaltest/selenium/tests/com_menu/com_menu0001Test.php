c44f9b503b057b32c2f0552aed3a99ab4cee9af7
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-09-07 15:06:54 +0000

    restructuring

--- testing/trunk/functionaltest/selenium/tests/com_menu/com_menu0001Test.php
+++ testing/trunk/functionaltest/selenium/tests/com_menu/com_menu0001Test.php
@@ -6,14 +6,79 @@ require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 
 class ComMenu0001 extends PHPUnit_Extensions_SeleniumTestCase
 {
-  function setUp()
+  function testCreateRemoveMenuTypesItems()
   {
     $cfg = new SeleniumConfig();
-    $this->setBrowser("*chrome");
-    $this->setBrowserUrl($cfg->host.$cfg->path);
+    print("Login back end" . "\n");
+    $this->open($cfg->path."administrator/index.php");
+    $this->type("modlgn_username", $cfg->username);
+    $this->type("modlgn_passwd", $cfg->password);
+    $this->click("//input[@value='Login']");
+    $this->waitForPageToLoad("30000");
+    print("Load menu manager" . "\n");
+    $this->click("link=Menu Manager");
+    $this->waitForPageToLoad("30000");
+    print("Create new menu" . "\n");
+    $this->click("//td[@id='toolbar-new']/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Fill in required fields and save" . "\n");
+    $this->type("menutype", "mytestmenu");
+    $this->type("title", "My Test Menu");
+    $this->type("description", "A Test Menu");
+    $this->type("module_title", "Test Menu");
+    $this->click("//td[@id='toolbar-save']/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Check that new menu is shown" . "\n");
+    $this->assertTrue($this->isElementPresent("link=My Test Menu"));
+    $this->assertTrue($this->isTextPresent("mytestmenu"));
+    print("Load module manager and check that new menu is shown" . "\n");
+    $this->click("link=Module Manager");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("link=Test Menu"));
+    print("Load front end and check that menu not shown" . "\n");
+    $this->open($cfg->path);
+    $this->assertNotEquals("Test Menu", $this->getText("//div[@id='leftcolumn']/div[1]/div/div/div/h3"));
+    print("Navigate to back end" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[2]/div/div/div/ul/li[7]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Load Module Manager" . "\n");
+    $this->click("link=Module Manager");
+    $this->waitForPageToLoad("30000");
+    print("Click Enabled button" . "\n");
+    $this->click("//img[@alt='Disabled']");
+    $this->waitForPageToLoad("30000");
+    print("Navigate to front end" . "\n");
+    $this->open($cfg->path);
+    print("Check that new menu is shown" . "\n");
+    $this->assertEquals("Test Menu", $this->getText("//div[@id='leftcolumn']/div[1]/div/div/div/h3"));
+    print("Navigate to back end" . "\n");
+    $this->click("//div[@id='leftcolumn']/div[3]/div/div/div/ul/li[7]/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Open Module Manager" . "\n");
+    $this->click("link=Module Manager");
+    $this->waitForPageToLoad("30000");
+    print("Select new module" . "\n");
+    $this->click("cb3");
+    print("click delete" . "\n");
+    $this->click("//td[@id='toolbar-delete']/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Open Menu Manager" . "\n");
+    $this->click("link=Menu Manager");
+    $this->waitForPageToLoad("30000");
+    print("Select new menu" . "\n");
+    $this->click("cb6");
+    print("Click delete" . "\n");
+    $this->click("//td[@id='toolbar-delete']/a/span");
+    $this->waitForPageToLoad("30000");
+    $this->click("//td[@id='toolbar-delete']/a/span");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=Logout");
+    $this->waitForPageToLoad("30000");
+    print("Finished com_menu0003.php" . "\n");
   }
 
-  function testMyTestCase()
+
+  function testCreateRemoveMenuItem()
   {
   	print("Starting com_menu0001.php" . "\n");
     $cfg = new SeleniumConfig();
@@ -81,6 +146,61 @@ class ComMenu0001 extends PHPUnit_Extensions_SeleniumTestCase
     $this->click("link=Return to site Home Page");
     $this->waitForPageToLoad("30000");
     $this->assertFalse($this->isElementPresent("//div[@id='leftcolumn']/div[1]/div/div/div/ul/li[9]/a/span"));
+    print("Finished com_menu0001.php" . "\n");
+  }
+
+  function testCreateRemoveMenuType()
+  {
+  	print("Starting com_menu0002.php" . "\n");
+    $cfg = new SeleniumConfig();
+
+    $this->open($cfg->path."administrator/index.php");
+    $this->type("modlgn_username", $cfg->username);
+    $this->type("modlgn_passwd", $cfg->password);
+    $this->click("//input[@value='Login']");
+    $this->waitForPageToLoad("30000");
+    print("Open Menu Manager" . "\n");
+    $this->click("link=Menu Manager");
+    $this->waitForPageToLoad("30000");
+    print("Create new menu" . "\n");
+    $this->click("//td[@id='toolbar-new']/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Enter required fields and save" . "\n");
+    $this->type("menutype", "selenium");
+    $this->type("title", "Selenium Menu");
+    $this->type("description", "Menu Description");
+    $this->type("module_title", "selmenu");
+    $this->click("//td[@id='toolbar-save']/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Check that menu text is present" . "\n");
+    $this->assertTrue($this->isTextPresent("Selenium Menu"));
+    $this->assertTrue($this->isTextPresent("selenium"));
+    print("Open Module Manager" . "\n");
+    $this->click("link=Module Manager");
+    $this->waitForPageToLoad("30000");
+    $this->select("limit", "label=100");
+    $this->waitForPageToLoad("30000");
+    print("Check that new menu is shown" . "\n");
+    $this->assertTrue($this->isTextPresent("selmenu"));
+    $this->click("link=Menu Manager");
+    $this->waitForPageToLoad("30000");
+    print("Select new menu and delete" . "\n");
+    $this->click("cb6");
+    $this->click("//td[@id='toolbar-delete']/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Check that selmenu selected" . "\n");
+    $this->assertTrue($this->isTextPresent("selmenu"));
+    print("Confirm delete" . "\n");
+    $this->click("//td[@id='toolbar-delete']/a/span");
+    $this->waitForPageToLoad("30000");
+    print("Check that menu is not shown" . "\n");
+    $this->assertFalse($this->isTextPresent("selenium"));
+    $this->click("link=Module Manager");
+    $this->waitForPageToLoad("30000");
+    $this->assertFalse($this->isTextPresent("selmenu"));
+    print("Logout" . "\n");
+    $this->click("link=Logout");
+    $this->waitForPageToLoad("30000");
     print("Finished com_menu0001.php" . "\n");
   }
 }
