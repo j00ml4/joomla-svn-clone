8dbe34c8d0b312fb8f6beb7570d7070a098c4d62
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-09-23 15:47:41 +0000

    Adding Refactored Unit Tests

---
+++ testing/branches/ian_trunk/unittest/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+define( '_JEXEC', 1 );
+
+define('JPATH_BASE', dirname(dirname(__FILE__)) );
+
+define( 'DS', DIRECTORY_SEPARATOR );
+
+require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
+require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
+
