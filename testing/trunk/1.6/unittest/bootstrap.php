a73b788292086409d4fa9bc8851a40bdb6034d2b
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-09-24 01:57:07 +0000

    Reduce bootstrap framework to absolute minimum.

--- testing/trunk/1.6/unittest/bootstrap.php
+++ testing/trunk/1.6/unittest/bootstrap.php
@@ -1,20 +1,33 @@
 <?php
 /**
- * @version	$Id$
+ * Prepares a minimalist framework for unit testing.
+ *
+ * Joomla is assumed to include the /unittest/ directory.
+ * eg, /path/to/joomla/unittest/
+ *
+ * @version		$Id$
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @link		http://www.phpunit.de/manual/current/en/installation.html
  */
 
-define('_JEXEC', 1);
+// Define expected Joomla constants.
 
-//
-// Joomla is assumed to include the /unittest/ directory
-// eg, /path/to/joomla/unittest/
-//
-define('JPATH_BASE', dirname(dirname(__FILE__)));
+define('DS',			DIRECTORY_SEPARATOR);
+define('_JEXEC',		1);
+define('JPATH_BASE',	dirname(dirname(__FILE__)));
 
-//
-// Include minimalist framework
-//
-define('DS', DIRECTORY_SEPARATOR);
+// Fix magic quotes.
 
-require_once (JPATH_BASE .DS.'includes'.DS.'defines.php');
-//require_once (JPATH_BASE .DS.'includes'.DS.'framework.php');
+@set_magic_quotes_runtime(0);
+
+// Maximise error reporting.
+
+@ini_set('zend.ze1_compatibility_mode', '0');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Include relative constants, JLoader and the jimport and jexit functions.
+
+require_once JPATH_BASE.DS.'includes'.DS.'defines.php';
+require_once JPATH_LIBRARIES.DS.'joomla'.DS.'import.php';
