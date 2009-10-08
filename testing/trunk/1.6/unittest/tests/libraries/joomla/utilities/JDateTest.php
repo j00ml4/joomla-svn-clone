ce5126b05ca0137e5d6c523d513bc6e64378234d
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-10-08 12:54:34 +0000

    Add skeleton for JDate tests.

---
+++ testing/trunk/1.6/unittest/tests/libraries/joomla/utilities/JDateTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @version		$Id$
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+class JDateTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+		jimport('joomla.utilities.date');
+	}
+
+	function tearDown()
+	{
+	}
+
+	public function testConstruct()
+	{
+		$date = new JDate;
+
+		$this->markTestIncomplete();
+	}
+
+	public function testGetSetOffset()
+	{
+		$this->markTestIncomplete();
+	}
+
+	public function testToFormat()
+	{
+		$this->markTestIncomplete();
+	}
+
+	public function testToISO8601()
+	{
+		$this->markTestIncomplete();
+	}
+
+	public function testToMySQL()
+	{
+		$this->markTestIncomplete();
+	}
+
+	public function testToRFC822()
+	{
+		$this->markTestIncomplete();
+	}
+
+	public function testToUnix()
+	{
+		$this->markTestIncomplete();
+	}
+}
