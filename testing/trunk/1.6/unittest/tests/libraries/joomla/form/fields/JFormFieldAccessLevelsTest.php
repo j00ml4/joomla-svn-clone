a85a7a0f3c769335e291a853869e9ff7c3a803f0
arlen <arlen@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-11-17 16:09:11 +0000

    Added placeholder test to kill warnings

--- testing/trunk/1.6/unittest/tests/libraries/joomla/form/fields/JFormFieldAccessLevelsTest.php
+++ testing/trunk/1.6/unittest/tests/libraries/joomla/form/fields/JFormFieldAccessLevelsTest.php
@@ -5,6 +5,7 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+require_once JPATH_BASE.'/libraries/joomla/form/formfield.php';
 require_once JPATH_BASE.'/libraries/joomla/form/fields/accesslevel.php';
 /**
  * Test class for JFormFieldAccessLevels.
@@ -15,20 +16,28 @@ class JFormFieldAccessLevelsTest extends PHPUnit_Framework_TestCase {
 	 * @var JFormFieldAccessLevels
 	 */
 	protected $object;
-	
+
 	/**
 	 * Sets up the fixture, for example, opens a network connection.
 	 * This method is called before a test is executed.
 	 */
 	protected function setUp() {
-		$this->object = new JFormFieldAccessLevels;
+		$this->object = new JFormFieldAccessLevel;
 	}
-	
+
 	/**
 	 * Tears down the fixture, for example, closes a network connection.
 	 * This method is called after a test is executed.
 	 */
 	protected function tearDown() {
+	}
+
+	/**
+	 * Empty test to prevent warnings.
+	 */
+	public function testNothing() {
+		// Remove the following lines when you implement this test.
+		$this->markTestSkipped('This test does nothing at all.');
 	}
 }
 ?>
--- testing/trunk/1.6/unittest/tests/libraries/joomla/form/fields/JFormFieldLanguagesTest.php
+++ testing/trunk/1.6/unittest/tests/libraries/joomla/form/fields/JFormFieldLanguagesTest.php
@@ -15,20 +15,28 @@ class JFormFieldLanguagesTest extends PHPUnit_Framework_TestCase {
 	 * @var JFormFieldLanguages
 	 */
 	protected $object;
-	
+
 	/**
 	 * Sets up the fixture, for example, opens a network connection.
 	 * This method is called before a test is executed.
 	 */
 	protected function setUp() {
-		$this->object = new JFormFieldLanguages;
+		$this->object = new JFormFieldLanguage;
 	}
-	
+
 	/**
 	 * Tears down the fixture, for example, closes a network connection.
 	 * This method is called after a test is executed.
 	 */
 	protected function tearDown() {
+	}
+
+	/**
+	 * Empty test to prevent warnings.
+	 */
+	public function testNothing() {
+		// Remove the following lines when you implement this test.
+		$this->markTestSkipped('This test does nothing at all.');
 	}
 }
 ?>
