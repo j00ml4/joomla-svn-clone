f1536dea9949d9ed33a08274d691728f616b6a4b
arlen <arlen@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-11-20 12:44:20 +0000

    Cleaned up and fixed session error

--- testing/trunk/1.6/unittest/tests/libraries/joomla/user/JAuthenticationTest.php
+++ testing/trunk/1.6/unittest/tests/libraries/joomla/user/JAuthenticationTest.php
@@ -87,7 +87,7 @@ class JAuthenticationTest extends PHPUnit_Framework_TestCase
     {
         // Remove the following lines when you implement this test.
         $this->markTestIncomplete(
-          'This test has not been implemented yet.'
+        	'This test has not been implemented yet.'
         );
     }
 }
