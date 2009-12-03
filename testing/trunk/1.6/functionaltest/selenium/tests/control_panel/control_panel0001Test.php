4ea3cfb4f0a5eb661101b51f04a06a27dc327dfa
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-12-03 11:37:12 +0000

    Test fixes.

--- testing/trunk/1.6/functionaltest/selenium/tests/control_panel/control_panel0001Test.php
+++ testing/trunk/1.6/functionaltest/selenium/tests/control_panel/control_panel0001Test.php
@@ -77,7 +77,7 @@ class ControlPanel0001 extends SeleniumJoomlaTestCase
 	echo "Check that Extensions menu options are visible\n";
 	$this->assertTrue($this->isElementPresent("link=Extension Manager"));
     $this->assertTrue($this->isElementPresent("link=Module Manager"));
-    $this->assertTrue($this->isElementPresent("link=Plugin Manager"));
+    $this->assertTrue($this->isElementPresent("link=Plug-in Manager"));
     $this->assertTrue($this->isElementPresent("link=Template Manager"));
     $this->assertTrue($this->isElementPresent("link=Language Manager"));
     echo "Check that Help menu options are visible\n";
