75b564887f453eb8b9052cae99aa4725739aad46
hackwar <hackwar@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2009-12-22 08:51:08 +0000

    Removing the now obsolete hackwar branch. It was fun while it lasted. ;-) Almost all code from this branch is implemented in the trunk now. The only thing missing is the voting plugin, but we'll tackle that one later. :-) Byebye

--- development/branches/hackwar/administrator/components/com_users/views/access/tmpl/default.php
+++
@@ -1,9 +0,0 @@
-<?php defined('_JEXEC') or die('Restricted access'); ?>
-
-<fieldset class="col width-40">
-<legend>Groups &amp; Users</legend>
-<div id="mytree"></div>
-<?php
-echo $this->usergrouphelper->getGroupTree($this->usergroups);
-?>
-</fieldset>
