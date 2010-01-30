907f0e6ad10b510e3ee03739da3a825200f4d8b1
dextercowley <dextercowley@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-01-30 17:46:02 +0000

    Fixes to contact articles, profile

--- development/branches/front_end_fixes/v11/components/com_contact/models/contact.php
+++ development/branches/front_end_fixes/v11/components/com_contact/models/contact.php
@@ -128,17 +128,16 @@ class ContactModelContact extends JModel
 				' ORDER BY state DESC, created DESC' ;
 			$this->_db->setQuery($query, 0, 10);
 			$articles = $this->_db->loadObjectList();
-			$contact->articles=$articles;
+			$result->articles = $articles;
 
 			//get the profile information for the linked user
-						$query = 'SELECT user_id, profile_key, profile_value, ordering' .
+			$query = 'SELECT user_id, profile_key, profile_value, ordering' .
 				' FROM #__user_profiles' .
 				' WHERE user_id = '.(int)$result->user_id .
-
 				' ORDER BY ordering ASC' ;
 			$this->_db->setQuery($query, 0, 10);
 			$profile = $this->_db->loadObjectList();
-			$contact->profile=$profile;
+			$result->profile = $profile;
 			
 		}
 		return $result;
