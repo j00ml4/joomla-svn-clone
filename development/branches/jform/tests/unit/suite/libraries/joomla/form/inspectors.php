79b41e1190da54fb1b6884d9b00275a4f3577703
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-02-04 20:40:02 +0000

    Working commit.

--- development/branches/jform/tests/unit/suite/libraries/joomla/form/inspectors.php
+++ development/branches/jform/tests/unit/suite/libraries/joomla/form/inspectors.php
@@ -6,28 +6,11 @@ require_once JPATH_BASE.'/libraries/joomla/form/form.php';
 
 class JFormInspector extends JForm
 {
-	public function &getName()
+	/**
+	 * @return	array	Return the protected options array.
+	 */
+	public function getOptions()
 	{
-		return $this->_name;
-	}
-
-	public function &getFieldsets()
-	{
-		return $this->_fieldsets;
-	}
-
-	public function &getGroups()
-	{
-		return $this->_groups;
-	}
-
-	public function &getData()
-	{
-		return $this->_data;
-	}
-
-	public function &getOptions()
-	{
-		return $this->_options;
+		return $this->options;
 	}
 }
