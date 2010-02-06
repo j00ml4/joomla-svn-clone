8ffbb11a30613a776234a7631d5238d4a5a575b8
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-02-06 00:05:08 +0000

    Working commit.

--- development/branches/jform/tests/unit/suite/libraries/joomla/form/inspectors.php
+++ development/branches/jform/tests/unit/suite/libraries/joomla/form/inspectors.php
@@ -36,4 +36,25 @@ class JFormInspector extends JForm
 	{
 		return $this->data;
 	}
+
+	public static function addNode(SimpleXMLElement $source, SimpleXMLElement $new)
+	{
+		return parent::addNode($source, $new);
+	}
+
+	public static function mergeNode(SimpleXMLElement $source, SimpleXMLElement $new)
+	{
+		return parent::mergeNode($source, $new);
+	}
+
+	public static function mergeNodes(SimpleXMLElement $source, SimpleXMLElement $new)
+	{
+		return parent::mergeNodes($source, $new);
+	}
+
+	public function loadFieldType($type, $new = true)
+	{
+		return parent::loadFieldType($type, $new);
+	}
+
 }
