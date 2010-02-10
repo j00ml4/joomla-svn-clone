f41b83b7c1fcc9bd3fa734c112132ad3024b90cb
klascommit <klascommit@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-02-10 19:59:19 +0000

    Add standalone gc() - garbage collect script to be used with local or remote crontab

--- development/branches/cache_klas/administrator/components/com_config/views/application/tmpl/default_cache.php
+++ development/branches/cache_klas/administrator/components/com_config/views/application/tmpl/default_cache.php
@@ -4,6 +4,7 @@
  * @package		Joomla.Administrator
  * @subpackage	com_config
  * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @copyright	Copyright (C) 2010 Klas Berlič
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
---
+++ development/branches/cache_klas/libraries/joomla/utilities/garbagecron.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* @copyright	Klas Berlič
+* @license		GNU/GPL
+*/
+
+// BEFORE USING RENAME THIS FILE TO SOMETHING UNIQUE !!!
+
+/* Initialize Joomla framework */
+define( '_JEXEC', 1 );
+define( 'DS', DIRECTORY_SEPARATOR );
+define('JPATH_BASE', dirname(__FILE__).DS.'..'.DS.'..'.DS.'..');
+/* Required Files */
+require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
+require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
+
+// Instantiate the application.
+$app = JFactory::getApplication('site');
+
+$cache = JFactory::getCache();
+$cache->gc();
