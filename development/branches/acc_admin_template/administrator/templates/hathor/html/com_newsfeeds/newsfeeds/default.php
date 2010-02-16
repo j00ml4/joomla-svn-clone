427dc66b40c8260109f9dd221bcdfde67f6dccd1
andrea.tarr <andrea.tarr@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-02-16 23:00:19 +0000

    Fix quickicon module override, add com_newsfeed overrides

---
+++ development/branches/acc_admin_template/administrator/templates/hathor/html/com_newsfeeds/newsfeeds/default.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * @version		$Id: default.php 14276 2010-01-18 14:20:28Z louis $
+ * @package		Joomla.Administrator
+ * @subpackage	com_newsfeeds
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+// Include the component HTML helpers.
+JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
+JHtml::_('behavior.tooltip');
+
+JHtml::_('script', 'multiselect.js');
+$user	= JFactory::getUser();
+$userId	= $user->get('id');
+?>
+
+<form action="<?php echo JRoute::_('index.php?option=com_newsfeeds&view=newsfeeds'); ?>" method="post" name="adminForm" id="adminForm">
+	<fieldset id="filter-bar">
+	<legend class="element-invisible"><?php echo JText::_('Filters'); ?></legend>
+		<div class="filter-search fltlft">
+			<label class="filter-search-lbl" for="filter_search"><?php echo JText::_('JSearch_Filter_Label'); ?></label>
+			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->state->get('filter.search'); ?>" title="<?php echo JText::_('Newsfeeds_Search_in_title'); ?>" />
+			<button type="submit"><?php echo JText::_('JSearch_Filter_Submit'); ?></button>
+			<button type="button" onclick="document.id('filter_search').value='';this.form.submit();"><?php echo JText::_('JSearch_Filter_Clear'); ?></button>
+		</div>
+		<div class="filter-select fltrt">
+			<label class="selectlabel" for="filter_access">
+				<?php echo JText::_('Filter_Access'); ?>
+			</label>
+			<select name="filter_access" class="inputbox" id="filter_access">
+				<option value=""><?php echo JText::_('JOption_Select_Access');?></option>
+				<?php echo JHtml::_('select.options', JHtml::_('access.assetgroups'), 'value', 'text', $this->state->get('filter.access'));?>
+			</select>
+
+			<label class="selectlabel" for="filter_published">
+				<?php echo JText::_('Filter_State'); ?>
+			</label> 
+			<select name="filter_published" class="inputbox" id="filter_published">
+				<option value=""><?php echo JText::_('JOption_Select_Published');?></option>
+				<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.published'), true);?>
+			</select>
+
+			<label class="selectlabel" for="filter_category_id">
+				<?php echo JText::_('Filter_Category'); ?>
+			</label>
+			<select name="filter_category_id" class="inputbox" id="filter_category_id">
+				<option value=""><?php echo JText::_('JOption_Select_Category');?></option>
+				<?php echo JHtml::_('select.options', JHtml::_('category.options', 'com_newsfeeds'), 'value', 'text', $this->state->get('filter.category_id'));?>
+			</select>
+			
+			<button type="button" id="filter-go" onclick="this.form.submit();">
+				<?php echo JText::_('Go'); ?></button>
+			
+		</div>
+	</fieldset>
+	<div class="clr"> </div>
+
+	<table class="adminlist">
+		<thead>
+			<tr>
+				<th class="checkmark-col">
+					<input type="checkbox" name="toggle" id="toggle" value="" title="<?php echo JText::_('Checkmark_All'); ?>" onclick="checkAll(this)" />
+				</th>
+				<th class="title">
+					<?php echo JHtml::_('grid.sort', 'JGrid_Heading_Title', 'a.name', $this->state->get('list.direction'), $this->state->get('list.ordering')); ?>
+				</th>
+				<th class="nowrap state-col">
+					<?php echo JHtml::_('grid.sort', 'JGrid_Heading_Published', 'a.published', $this->state->get('list.direction'), $this->state->get('list.ordering')); ?>
+				</th>
+				<th class="nowrap title category-col">
+					<?php echo JHtml::_('grid.sort',  'JGrid_Heading_Category', 'category_title', $this->state->get('list.direction'), $this->state->get('list.ordering')); ?>
+				</th>
+				<th class="nowrap ordering-col"">
+					<?php echo JHtml::_('grid.sort',  'JGrid_Heading_Ordering', 'a.ordering', $this->state->get('list.direction'), $this->state->get('list.ordering')); ?>
+					<?php echo JHtml::_('grid.order',  $this->items, 'filesave.png', 'newsfeeds.saveorder'); ?>
+				</th>
+				<th class="title access-col">
+					<?php echo JHtml::_('grid.sort', 'JGrid_Heading_Access', 'a.access', $this->state->get('list.direction'), $this->state->get('list.ordering')); ?>
+				</th>
+				<th class="width-10">
+					<?php echo JHtml::_('grid.sort',  'Newsfeeds_Num_Articles_Heading', 'numarticles', $this->state->get('list.direction'), $this->state->get('list.ordering')); ?>
+				</th>
+				<th class="width-5">
+					<?php echo JHtml::_('grid.sort', 'Newsfeeds_Cache_Time_Heading', 'a.hits', $this->state->get('list.direction'), $this->state->get('list.ordering')); ?>
+				</th>
+				<th class="width-5">
+					<?php echo JHtml::_('grid.sort', 'JGrid_Heading_Language', 'a.language', $this->state->get('list.direction'), $this->state->get('list.ordering')); ?>
+				</th>
+				<th class="nowrap id-col">
+					<?php echo JHtml::_('grid.sort', 'JGrid_Heading_ID', 'a.id', $this->state->get('list.direction'), $this->state->get('list.ordering')); ?>
+				</th>
+			</tr>
+		</thead>
+		
+		<tbody>
+		<?php foreach ($this->items as $i => $item) :
+			$ordering	= ($this->state->get('list.ordering') == 'a.ordering');
+			$canCreate	= $user->authorise('core.create',		'com_newsfeeds.category.'.$item->catid);
+			$canEdit	= $user->authorise('core.edit',			'com_newsfeeds.category.'.$item->catid);
+			$canChange	= $user->authorise('core.edit.state',	'com_newsfeeds.category.'.$item->catid);
+			?>
+			<tr class="row<?php echo $i % 2; ?>">
+				<th class="center">
+					<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+				</th>
+				<td>
+					<?php if ($item->checked_out) : ?>
+						<?php echo JHtml::_('jgrid.checkedout', $item->editor, $item->checked_out_time); ?>
+					<?php endif; ?>
+					<?php if ($canCreate || $canEdit) : ?>
+						<a href="<?php echo JRoute::_('index.php?option=com_newsfeeds&task=newsfeed.edit&id='.(int) $item->id); ?>">
+							<?php echo $this->escape($item->name); ?></a>
+					<?php else : ?>
+							<?php echo $this->escape($item->name); ?>
+					<?php endif; ?>
+					<p class="smallsub">
+						(<span><?php echo JText::_('JFIELD_ALIAS_LABEL'); ?>:</span> <?php echo $this->escape($item->alias);?>)</p>
+				</td>
+				<td class="center">
+					<?php echo JHtml::_('jgrid.published', $item->published, $i, 'newsfeeds.', $canChange); ?>
+				</td>
+				<td class="center">
+					<?php echo $this->escape($item->category_title); ?>
+				</td>
+				<td class="order">
+					<?php if ($canChange) : ?>
+						<span><?php echo $this->pagination->orderUpIcon($i, true, 'newsfeeds.orderup', 'JGrid_Move_Up', $ordering); ?></span>
+						<span><?php echo $this->pagination->orderDownIcon($i, $this->pagination->total, true, 'newsfeeds.orderdown', 'JGrid_Move_Down', $ordering); ?></span>
+						<?php $disabled = $ordering ?  '' : 'disabled="disabled"'; ?>
+						<input type="text" name="order[]" size="5" value="<?php echo $item->ordering;?>" <?php echo $disabled ?> class="text-area-order" title="<?php echo $item->name; ?> order"/>
+					<?php else : ?>
+						<?php echo $item->ordering; ?>
+					<?php endif; ?>
+				</td>
+				<td class="center">
+					<?php echo $this->escape($item->access_level); ?>
+				</td>
+				<td class="center">
+					<?php echo (int) $item->numarticles; ?>
+				</td>
+				<td class="center">
+					<?php echo (int) $item->cache_time; ?>
+				</td>
+				<td class="center">
+					<?php echo $this->escape($item->language); ?>
+				</td>
+				<td class="center">
+					<?php echo (int) $item->id; ?>
+				</td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+	<?php echo $this->pagination->getListFooter(); ?>
+
+	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="boxchecked" value="0" />
+	<input type="hidden" name="filter_order" value="<?php echo $this->state->get('list.ordering'); ?>" />
+	<input type="hidden" name="filter_order_Dir" value="<?php echo $this->state->get('list.direction'); ?>" />
+	<?php echo JHtml::_('form.token'); ?>
+</form>
