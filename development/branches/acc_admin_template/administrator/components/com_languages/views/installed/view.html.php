1453a55461feb082cb05d3669659e482477c1dbe
dextercowley <dextercowley@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-02-18 12:40:39 +0000

    Merge from trunk through rev 14901

--- development/branches/acc_admin_template/administrator/components/com_languages/views/installed/view.html.php
+++ development/branches/acc_admin_template/administrator/components/com_languages/views/installed/view.html.php
@@ -83,8 +83,8 @@ class LanguagesViewInstalled extends JView
 	 */
 	protected function _setToolBar()
 	{
-		JToolBarHelper::title(JText::_('Langs_View_Installed_Title'), 'langmanager.png');
-		JToolBarHelper::makeDefault('installed.publish');
+		JToolBarHelper::title(JText::_('COM_LANGS_VIEW_INSTALLED_TITLE'), 'langmanager.png');
+		JToolBarHelper::makeDefault('installed.publish','JTOOLBAR_DEFAULT');
 		JToolBarHelper::divider();
 		JToolBarHelper::preferences('com_languages');
 		JToolBarHelper::divider();
