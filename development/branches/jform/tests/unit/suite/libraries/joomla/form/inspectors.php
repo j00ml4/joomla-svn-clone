d283c279235f1763d2a7ec8c803f7be9c56b8b98
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-02-19 00:58:12 +0000

    Update tests to reflect changes in API

--- development/branches/jform/tests/unit/suite/libraries/joomla/form/inspectors.php
+++ development/branches/jform/tests/unit/suite/libraries/joomla/form/inspectors.php
@@ -6,37 +6,6 @@ require_once JPATH_BASE.'/libraries/joomla/form/form.php';
 
 class JFormInspector extends JForm
 {
-	/**
-	 * @return	array	Return the protected options array.
-	 */
-	public function getOptions()
-	{
-		return $this->options;
-	}
-
-	/**
-	 * @return	array	Return the protected data object.
-	 */
-	public function getXML()
-	{
-		return $this->xml;
-	}
-
-	public function getFieldsByGroup($name)
-	{
-		return parent::getFieldsByGroup($name);
-	}
-
-	public function getFieldsByFieldset($name)
-	{
-		return parent::getFieldsByFieldset($name);
-	}
-
-	public function getData()
-	{
-		return $this->data;
-	}
-
 	public static function addNode(SimpleXMLElement $source, SimpleXMLElement $new)
 	{
 		return parent::addNode($source, $new);
@@ -52,13 +21,41 @@ class JFormInspector extends JForm
 		return parent::mergeNodes($source, $new);
 	}
 
-	public function loadFieldType($type, $new = true)
-	{
-		return parent::loadFieldType($type, $new);
-	}
-
 	public function findField($name, $group)
 	{
 		return parent::findField($name, $group);
+	}
+
+	public function findFieldsByGroup($name)
+	{
+		return parent::findFieldsByGroup($name);
+	}
+
+	public function findFieldsByFieldset($name)
+	{
+		return parent::findFieldsByFieldset($name);
+	}
+
+	public function getData()
+	{
+		return $this->data;
+	}
+
+	/**
+	 * @return	array	Return the protected options array.
+	 */
+	public function getOptions()
+	{
+		return $this->options;
+	}
+
+	public function getXML()
+	{
+		return $this->xml;
+	}
+
+	public function loadFieldType($type, $new = true)
+	{
+		return parent::loadFieldType($type, $new);
 	}
 }
