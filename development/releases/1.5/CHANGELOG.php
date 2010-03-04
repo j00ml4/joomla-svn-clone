f0771389b9e6109f9dd6a92298d9d77ef50718bb
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-03-04 21:49:55 +0000

    # [#19798] Undefined variable notice when enabling or disabling a user in com_user

--- development/releases/1.5/CHANGELOG.php
+++ development/releases/1.5/CHANGELOG.php
@@ -40,6 +40,7 @@ $ -> Language fix or change
 04-Mar-2010 Ian MacLennan
  $ [#19836] New Arabic Unitag installation language ar_AA
  # [#19831] Preventing mod_login redirect to display trashed menu items
+ # [#19798] Undefined variable notice when enabling or disabling a user in com_user
 
 27-Feb-2010 Ian MacLennan
  # [#19816] Adding a missing string in many installation ini, added helpfile listing
