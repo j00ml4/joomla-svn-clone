5239d37ef22c742ef4b985ec6aa2372987d7311f
tony <tony@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-03-06 18:00:50 +0000

    Bug fixing in com_users

--- development/branches/new_user_approval/components/com_users/views/resend/tmpl/default.php
+++ development/branches/new_user_approval/components/com_users/views/resend/tmpl/default.php
@@ -8,24 +8,31 @@
  */
 
 defined('_JEXEC') or die;
+
+JHtml::_('behavior.mootools');
+JHtml::_('behavior.tooltip');
+JHtml::_('behavior.formvalidation');
 ?>
 
-<form action="<?php echo $this->form->getAction(); ?>" method="post">
+<form id="member-registration" action="<?php echo JRoute::_('index.php?option=com_users&task=resend.resend'); ?>" method="post" class="form-validate">
 
-	<?php foreach ($this->form->getGroups() as $group): ?>
+	<?php
+	// Iterate through the form fieldsets and display each one.
+	foreach ($this->form->getFieldsets() as $group => $fieldset):
+	?>
 	<fieldset>
 		<dl>
 		<?php foreach ($this->form->getFields($group, $group) as $name => $field): ?>
 			<dt><?php echo $field->label; ?></dt>
-			<dd><?php echo $field->field; ?></dd>
+			<dd><?php echo $field->input; ?></dd>
 		<?php endforeach; ?>
 		</dl>
 	</fieldset>
-	<?php endforeach; ?>
+<?php endforeach; ?>
 
-	<button type="submit">Submit</button>
+	<button type="submit"><?php echo JText::_('BUTTON_SUBMIT'); ?></button>
 
 	<input type="hidden" name="option" value="com_users" />
-	<input type="hidden" name="task" value="registration.profile" />
+	<input type="hidden" name="task" value="member.resend" />
 	<?php echo JHtml::_('form.token'); ?>
 </form>
