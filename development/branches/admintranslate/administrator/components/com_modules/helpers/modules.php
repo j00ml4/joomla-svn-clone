ae4d85d6e1e3a63973790d01a6e49a6c2c2c51b3
chdemko <chdemko@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-03-08 11:00:21 +0000

    Merge with trunk

---
+++ development/branches/admintranslate/administrator/components/com_modules/helpers/modules.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * @version		$Id$
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+/**
+ * Modules component helper.
+ *
+ * @package		Joomla.Administrator
+ * @subpackage	com_modules
+ * @since		1.6
+ */
+class ModulesHelper
+{
+	/**
+	 * Configure the Linkbar.
+	 *
+	 * @param	string	The name of the active view.
+	 */
+	public static function addSubmenu($vName)
+	{
+		// Not used in this component.
+	}
+
+	/**
+	 * Gets a list of the actions that can be performed.
+	 *
+	 * @return	JObject
+	 */
+	public static function getActions()
+	{
+		$user	= JFactory::getUser();
+		$result	= new JObject;
+
+		$actions = array(
+			'core.admin', 'core.manage', 'core.create', 'core.edit', 'core.edit.state', 'core.delete'
+		);
+
+		foreach ($actions as $action) {
+			$result->set($action, $user->authorise($action, 'com_modules'));
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Get a list of filter options for the state of a module.
+	 *
+	 * @return	array	An array of JHtmlOption elements.
+	 */
+	static function getStateOptions()
+	{
+		// Build the filter options.
+		$options	= array();
+		$options[]	= JHtml::_('select.option',	'1',	JText::_('JOption_Enabled'));
+		$options[]	= JHtml::_('select.option',	'0',	JText::_('JOption_Disabled'));
+		$options[]	= JHtml::_('select.option',	'-2',	JText::_('JOption_Trash'));
+		return $options;
+	}
+
+	/**
+	 * Get a list of filter options for the application clients.
+	 *
+	 * @return	array	An array of JHtmlOption elements.
+	 */
+	static function getClientOptions()
+	{
+		// Build the filter options.
+		$options	= array();
+		$options[]	= JHtml::_('select.option', '0', JText::_('COM_MODULES_OPTION_SITE'));
+		$options[]	= JHtml::_('select.option', '1', JText::_('COM_MODULES_OPTION_ADMINISTRATOR'));
+		return $options;
+	}
+
+	static function getPositions($clientId)
+	{
+		jimport('joomla.filesystem.folder');
+
+		$db		= JFactory::getDbo();
+		$query	= $db->getQuery(true);
+
+		$query->select('DISTINCT(position)');
+		$query->from('#__modules');
+		$query->where('`client_id` = '.(int) $clientId);
+		$query->order('position');
+
+		$db->setQuery($query);
+		$positions = $db->loadResultArray();
+		$positions = (is_array($positions)) ? $positions : array();
+
+		if ($error = $db->getErrorMsg()) {
+			JError::raiseWarning(500, $error);
+			return;
+		}
+
+		// Build the list
+		$options = array();
+		foreach ($positions as $position) {
+			$options[]	= JHtml::_('select.option', $position, $position);
+		}
+		return $options;
+	}
+
+	/**
+	 * Get a list of the unique modules installed in the client application.
+	 *
+	 * @param	int		The client id.
+	 *
+	 * @return	array
+	 */
+	public static function getModules($clientId)
+	{
+		$db		= JFactory::getDbo();
+		$query	= $db->getQuery(true);
+
+		$query->select('DISTINCT(m.module) AS value, e.name AS text');
+		$query->from('#__modules AS m');
+		$query->join('LEFT', '#__extensions AS e ON e.element=m.module');
+		$query->where('m.`client_id` = '.(int)$clientId);
+
+		$db->setQuery($query);
+		$modules = $db->loadObjectList();
+		foreach ($modules as $i=>$module) {
+			$extension = $module->value;
+			$path = $clientId ? JPATH_ADMINISTRATOR : JPATH_SITE;
+			$source = $path . "/modules/$extension";
+			$lang = JFactory::getLanguage();
+				$lang->load("$extension.manage", $path, null, false, false)
+			||	$lang->load($extension, $path, null, false, false)
+			||	$lang->load("$extension.manage", $source, null, false, false)
+			||	$lang->load($extension, $source, null, false, false)
+			||	$lang->load("$extension.manage", $path, $lang->getDefault(), false, false)
+			||	$lang->load($extension, $path, $lang->getDefault(), false, false)
+			||	$lang->load("$extension.manage", $source, $lang->getDefault(), false, false)
+			||	$lang->load($extension, $source, $lang->getDefault(), false, false);
+			$modules[$i]->text = JText::_($module->text);
+		}
+		JArrayHelper::sortObjects($modules,'text');
+		return $modules;
+	}
+
+	/**
+	 * Get a list of the assignment options for modules to menus.
+	 *
+	 * @param	int		The client id.
+	 *
+	 * @return	array
+	 */
+	public static function getAssignmentOptions($clientId)
+	{
+		$options = array();
+		$options[] = JHtml::_('select.option', '0', 'Modules_Option_Menu_All');
+		$options[] = JHtml::_('select.option', '-', 'Modules_Option_Menu_None');
+
+		if ($clientId == 0) {
+			$options[] = JHtml::_('select.option', '1', 'Modules_Option_Menu_Include');
+			$options[] = JHtml::_('select.option', '-1', 'Modules_Option_Menu_Exclude');
+		}
+
+		return $options;
+	}
+}
