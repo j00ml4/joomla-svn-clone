29fa15e8a0d2d45f97900b3336f571a15f4582ae
chdemko <chdemko@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-03-16 09:42:15 +0000

    Creating lang switcher branch

---
+++ development/branches/langswitcher/modules/mod_languages/tmpl/default.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Site
+ * @subpackage	mod_languages
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+//$uri = JFactory::getURI();
+?>
+<form action="<?php echo JRoute::_('index.php?option=com_users'); ?>" method="post" id="mod_languages-form">
+	<div class="mod_languages<?php echo $params->get('moduleclass_sfx') ?>">
+	<?php if ($headerText) : ?>
+		<div class="mod_languages_header"><?php echo $headerText; ?></div>
+	<?php endif; ?>
+	<?php echo JHtml::_('select.genericlist', $list, 'language', ' onchange="this.form.submit();"','value','text',$tag);?>
+	<?php if ($footerText) : ?>
+		<div class="mod_languages_footer"><?php echo $footerText; ?></div>
+	<?php endif; ?>
+	</div>
+	<?php foreach($list as $language):?>
+	<input type="hidden" name="redirect[<?php echo $language['value'];?>]" value="<?php echo base64_encode($language['redirect']);?>" />
+	<?php endforeach;?>
+	<input type="hidden" name="task" value="profile.language" />
+	<?php echo JHtml::_('form.token'); ?>	
+</form>
+
