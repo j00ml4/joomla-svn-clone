b1dd2a34c0bd6a9eec30f1e9017f841532f977cc
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-03-22 19:20:07 +0000

    Fix up component config form loader.

--- development/branches/jform/administrator/components/com_config/models/component.php
+++ development/branches/jform/administrator/components/com_config/models/component.php
@@ -7,7 +7,7 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// No direct access
+// No direct access.
 defined('_JEXEC') or die;
 
 jimport('joomla.application.component.modelform');
@@ -30,8 +30,7 @@ class ConfigModelComponent extends JModelForm
 		$this->setState('component.option', $component);
 
 		// Set an alternative path for the configuration file.
-		if ($path = JRequest::getString('path'))
-		{
+		if ($path = JRequest::getString('path')) {
 			$path = JPath::clean(JPATH_SITE.DS.$path);
 			JPath::check($path);
 			$this->setState('component.path', $path);
@@ -46,22 +45,20 @@ class ConfigModelComponent extends JModelForm
 	 */
 	public function getForm()
 	{
-		$option = $this->getState('component.option');
-		$path	= $this->getState('component.path', '');
 		jimport('joomla.form.form');
 
-		if ($path){
+		if ($path = $this->getState('component.path')) {
 			// Add the search path for the admin component config.xml file.
 			JForm::addFormPath($path);
 		} else {
 			// Add the search path for the admin component config.xml file.
-			JForm::addFormPath(JPATH_ADMINISTRATOR.'/components/'.$option);
+			JForm::addFormPath(JPATH_ADMINISTRATOR.'/components/'.$this->getState('component.option'));
 		}
 
 		// Get the form.
 		$form = parent::getForm(
-			'config',
 			'com_config.component',
+			'config',
 			array('control' => 'jform', 'event' => 'onPrepareForm'),
 			false,
 			'/config'
@@ -111,13 +108,12 @@ class ConfigModelComponent extends JModelForm
 		$table	= &JTable::getInstance('extension');
 
 		// Save the rules.
-		if (isset($data['params']) && isset($data['params']['rules']))
-		{
+		if (isset($data['params']) && isset($data['params']['rules'])) {
 			jimport('joomla.access.rules');
 			$rules	= new JRules($data['params']['rules']);
 			$asset	= JTable::getInstance('asset');
-			if (!$asset->loadByName($data['option']))
-			{
+
+			if (!$asset->loadByName($data['option'])) {
 				$root	= JTable::getInstance('asset');
 				$root->loadByName('root.1');
 				$asset->name = $data['option'];
@@ -125,8 +121,8 @@ class ConfigModelComponent extends JModelForm
 				$asset->setLocation($root->id,'last-child');
 			}
 			$asset->rules = (string) $rules;
-			if (!$asset->check() || !$asset->store())
-			{
+
+			if (!$asset->check() || !$asset->store()) {
 				$this->setError($asset->getError());
 				return false;
 			}
@@ -136,30 +132,26 @@ class ConfigModelComponent extends JModelForm
 		}
 
 		// Load the previous Data
-		if (!$table->load($data['id']))
-		{
+		if (!$table->load($data['id'])) {
 			$this->setError($table->getError());
 			return false;
 		}
 		unset($data['id']);
 
 		// Bind the data.
-		if (!$table->bind($data))
-		{
+		if (!$table->bind($data)) {
 			$this->setError($table->getError());
 			return false;
 		}
 
 		// Check the data.
-		if (!$table->check())
-		{
+		if (!$table->check()) {
 			$this->setError($table->getError());
 			return false;
 		}
 
 		// Store the data.
-		if (!$table->store())
-		{
+		if (!$table->store()) {
 			$this->setError($table->getError());
 			return false;
 		}
@@ -170,5 +162,4 @@ class ConfigModelComponent extends JModelForm
 
 		return true;
 	}
-
 }
