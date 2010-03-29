fe0f3d8d2cafd85b2f7108c04fb2c597bbcca9e9
a.radtke <a.radtke@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-03-29 08:46:10 +0000

    care about component.php css fixed

--- development/branches/beez/templates/beez_20/component.php
+++ development/branches/beez/templates/beez_20/component.php
@@ -11,19 +11,26 @@
 
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
+$color = $this->params->get('templatecolor');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<jdoc:include type="head" />
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez/css/template.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/layout.css" type="text/css" media="screen,projection" />
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/print.css" type="text/css" media="Print" />
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/<?php echo $color; ?>.css" type="text/css" />
+
 
 <?php if($this->direction == 'rtl') : ?>
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez/css/template_rtl.css" type="text/css" />
 <?php endif; ?>
 </head>
 <body class="contentpane">
-	<jdoc:include type="message" />
-	<jdoc:include type="component" />
+	<div id="all">
+		<jdoc:include type="message" />
+		<jdoc:include type="component" />
+	</div>
 </body>
 </html>
