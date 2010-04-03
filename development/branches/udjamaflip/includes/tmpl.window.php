70389a30be3950de10318dd408cac80d7a352a93
udjamaflip <udjamaflip@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-03 05:49:11 +0000

    Clean up, adding 1.6 and 1.5 seperate versions.

--- development/branches/udjamaflip/includes/tmpl.window.php
+++
@@ -1,8 +0,0 @@
-﻿<?php if (!empty($_GET['udjaurl']))	{ ?> 
-	<script type="text/javascript">
-	jQuery.noConflict();
-	jQuery(document).ready(function ($) {
-			addWindow(1, 2, '<?php echo $_GET['udjaurl']; ?>', jQuery.udjaParams.width+'-'+jQuery.udjaParams.height+'-'+jQuery.udjaParams.xPosition+'-'+jQuery.udjaParams.yPosition, '');
-		});
-	</script>
-<?php } ?>
