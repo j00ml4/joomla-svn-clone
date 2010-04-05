92203f61283b1ec60e3a1de15b3e50df7e1a5c46
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-05 18:20:24 +0000

    Comments plugin not needed.

--- development/branches/comments/installation/sql/mysql/joomla.sql
+++ development/branches/comments/installation/sql/mysql/joomla.sql
@@ -481,8 +481,7 @@ INSERT INTO `#__extensions` VALUES
 (108, 'mod_comments_latest', 'module', 'mod_comments_latest', '', 0, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0),
 (109, 'mod_comments_most_commented', 'module', 'mod_comments_most_commented', '', 0, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0),
 (110, 'mod_comments_rating', 'module', 'mod_comments_rating', '', 0, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0),
-(111, 'mod_comments_share', 'module', 'mod_comments_share', '', 0, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0),
-(112, 'plg_comments_comments', 'plugin', 'comments', 'content', 0, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0);
+(111, 'mod_comments_share', 'module', 'mod_comments_share', '', 0, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0);
 
 # -------------------------------------------------------
 
