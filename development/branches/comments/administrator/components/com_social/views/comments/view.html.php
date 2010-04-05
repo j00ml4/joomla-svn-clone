1198ac7a1bd8e4de2fb2f117385c25cd49db6894
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-05 19:36:54 +0000

    First pass renaming comments suit to "social".

---
+++ development/branches/comments/administrator/components/com_social/views/comments/view.html.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * @version		$Id$
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.view');
+
+/**
+ * View class for a list of comments.
+ *
+ * @package		Joomla.Administrator
+ * @subpackage	com_comments
+ * @since		1.6
+ */
+class CommentsViewComments extends JView
+{
+	protected $items;
+	protected $pagination;
+	protected $state;
+
+	/**
+	 * Display the view
+	 */
+	public function display($tpl = null)
+	{
+		$this->items		= $this->get('Items');
+		$this->pagination	= $this->get('Pagination');
+		$this->state		= $this->get('State');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			JError::raiseError(500, implode("\n", $errors));
+			return false;
+		}
+
+		parent::display($tpl);
+		$this->setToolbar();
+	}
+
+	function getContentRoute($url)
+	{
+		static $router;
+
+		// Only get the router once.
+		if (!is_object($router))
+		{
+			// Import dependencies.
+			jimport('joomla.application.router');
+			require_once(JPATH_SITE.DS.'includes'.DS.'application.php');
+
+			// Get the site router.
+			$config	= JFactory::getConfig();
+			$router = JRouter::getInstance('site');
+			$router->setMode($config->get('sef', 1));
+		}
+
+		// Build the route.
+		$uri	= $router->build($url);
+		$route	= $uri->toString(array('path', 'query', 'fragment'));
+
+		// Strip out the base portion of the route.
+		$route = str_replace('administrator/', '', $route);
+
+		return $route;
+	}
+
+	/**
+	 * Setup the Toolbar.
+	 */
+	protected function setToolbar()
+	{
+		$state	= $this->get('State');
+		$canDo	= CommentsHelper::getActions($state->get('filter.category_id'));
+
+		JToolBarHelper::title('Comments: '.JText::_('COMMENTS_MODERATE_COMMENTS_TITLE'), 'logo');
+
+		$toolbar = JToolBar::getInstance('toolbar');
+		$toolbar->appendButton('Standard', 'save', 'COMMENTS_MODERATE', 'comment.moderate', false, false);
+
+		if ($canDo->get('core.admin')) {
+			JToolBarHelper::preferences('com_comments');
+		}
+		JToolBarHelper::help('screen.comments','JTOOLBAR_HELP');
+	}
+}
