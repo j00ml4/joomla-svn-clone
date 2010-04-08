e673db3ff4ac93b4827d6cbced9bbd5c0996cd49
klascommit <klascommit@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-08 16:20:11 +0000

    Add new caching to mod_stats, mod_users_latest, mod_weblinks, mod_wrapper, mod_articles_category, mod_breadcrumbs Mod_menu cache mode change to itemid Re-bugfix mode_search itemid bug already fixed in 1.5.

--- development/branches/cache_klas/modules/mod_menu/mod_menu.php
+++ development/branches/cache_klas/modules/mod_menu/mod_menu.php
@@ -13,24 +13,15 @@ defined('_JEXEC') or die;
 // Include the syndicate functions only once
 require_once dirname(__FILE__).DS.'helper.php';
 
-//$list = modMenuHelper::getList($params);
 $menu	= &JSite::getMenu();
 $active	= $menu->getActive();
 $active_id = isset($active) ? $active->id : $menu->getDefault()->id;
 $path	= isset($active) ? $active->tree : array();
 $showAll	= $params->get('showAllChildren');
 
-$cacheid = md5(serialize(array ($active_id,$path,$params,$module->id)));
-
-$cacheparams = new stdClass;
-$cacheparams->cachemode = 'id';
-$cacheparams->class = 'modMenuHelper';	
-$cacheparams->method = 'getList';
-$cacheparams->methodparams = $params;
-$cacheparams->modeparams = $cacheid;
-$cacheparams->cachegroup = 'com_modules';
 
 
-$list = JModuleHelper::ModuleCache ($module, $params, $cacheparams);
+$list = modMenuHelper::getList($params);
+
 
 require JModuleHelper::getLayoutPath('mod_menu', $params->get('layout', 'default'));
