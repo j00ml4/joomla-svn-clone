9f999d8fda1b9f8cf36561a424114db2235f57f5
klascommit <klascommit@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-09 03:30:22 +0000

    Merge in trunk up to 15963

--- development/branches/cache_klas/logs/error.php
+++
@@ -1,7 +0,0 @@
-#<?php die('Direct Access To Log Files Not Permitted'); ?>
-#Version: 1.0
-#Date: 2010-03-29 09:02:06
-#Fields: date	time	level	c-ip	status	comment
-#Software: Joomla! 1.6.0 Alpha [ Hope ] 22-June-2009 23:00 GMT
-2010-03-29	09:02:06	-	127.0.0.1	Joomla FAILURE: 	Invalid password
-2010-03-29	09:02:09	-	127.0.0.1	Joomla FAILURE: 	Empty password not allowed
