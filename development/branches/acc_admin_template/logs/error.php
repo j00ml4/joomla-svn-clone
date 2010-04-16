87a823415665e16f2e2660790ae1e7bc930ace2f
andrea.tarr <andrea.tarr@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-16 22:00:34 +0000

    Get rid of error log

--- development/branches/acc_admin_template/logs/error.php
+++
@@ -1,6 +0,0 @@
-#<?php die('Direct Access To Log Files Not Permitted'); ?>
-#Version: 1.0
-#Date: 2010-04-14 12:59:30
-#Fields: date	time	level	c-ip	status	comment
-#Software: Joomla! 1.6.0 Alpha [ Hope ] 22-June-2009 23:00 GMT
-2010-04-14	12:59:30	-	127.0.0.1	Joomla FAILURE: 	User does not exist
