3f787697a51293b87ff8d816a51aa3d7946a13c8
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-22 17:43:58 +0000

    Convert mod_social_comment to use JForm

--- development/branches/comments/modules/mod_social_comment/tmpl/form.php
+++ development/branches/comments/modules/mod_social_comment/tmpl/form.php
@@ -10,39 +10,37 @@
 defined('_JEXEC') or die;
 ?>
 
-<form id="respond-form" method="post" action="<?php echo JRoute::_('index.php?option=com_social&task=comment.add'); ?>" class="form-validate">
+<form id="respond-form" method="post" action="<?php echo JRoute::_('index.php?option='.$extension.'&task=social.addcomment'); ?>" class="form-validate">
 	<fieldset>
 		<dl>
 <?php if (!$user->get('id')) : ?>
-			<dt><label for="comment_name"><?php echo JText::_('MOD_SOCIAL_COMMENT_NAME');?></label></dt>
-			<dd><input id="comment_name" name="name" class="inputbox required" tabindex="1" /></dd>
+			<dt><?php echo $form->getLabel('name'); ?></dt>
+			<dd><?php echo $form->getInput('name'); ?></dd>
 <?php endif; ?>
 
 <?php if (!$user->get('id')) : ?>
-			<dt><label for="comment_email"><?php echo JText::_('MOD_SOCIAL_COMMENT_EMAIL');?></label></dt>
-			<dd><input id="comment_email" name="email" class="inputbox required validate-email" tabindex="2" /></dd>
+			<dt><?php echo $form->getLabel('email'); ?></dt>
+			<dd><?php echo $form->getInput('email'); ?></dd>
 <?php endif; ?>
 
 <?php if ($params->get('enable_website', 1)) : ?>
-			<dt><label for="comment_url"><?php echo JText::_('MOD_SOCIAL_COMMENT_WEBSITE');?></label></dt>
-			<dd><input id="comment_url" name="url" class="inputbox" tabindex="3" /></dd>
+			<dt><?php echo $form->getLabel('url'); ?></dt>
+			<dd><?php echo $form->getInput('url'); ?></dd>
 <?php endif; ?>
 
 <?php if ($params->get('enable_subject', 1)) : ?>
-			<dt><label for="comment_subject"><?php echo JText::_('MOD_SOCIAL_COMMENT_SUBJECT');?></label></dt>
-			<dd><input id="comment_subject" name="subject" class="inputbox" tabindex="4" value="<?php echo $params->get('title'); ?>" /></dd>
+			<dt><?php echo $form->getLabel('subject'); ?></dt>
+			<dd><?php echo $form->getInput('subject'); ?></dd>
 <?php endif; ?>
 
-			<dt><label for="comment_body"><?php echo JText::_('MOD_SOCIAL_COMMENT_COMMENT');?></label></dt>
-			<dd>
-				<textarea id="comment_body" name="body" class="bbcode required" tabindex="5" width="100%" height="200px"></textarea>
-			</dd>
+			<dt><?php echo $form->getLabel('body'); ?></dt>
+			<dd><?php echo $form->getInput('body'); ?></dd>
 		</dl>
 
 		<input type="submit" name="button" value="<?php echo JText::_('MOD_SOCIAL_COMMENT_SUBMIT');?>" id="submitter" tabindex="7" />
 
-		<input type="hidden" name="thread_id" value="<?php echo $thread->id; ?>" />
-		<input type="hidden" name="redirect" value="<?php echo base64_encode($uri->toString(array('path', 'query', 'fragment'))); ?>" />
+		<?php echo $form->getInput('context'); ?>
+		<?php echo $form->getInput('redirect'); ?>
 		<?php echo JHtml::_('form.token'); ?>
 	</fieldset>
 </form>
