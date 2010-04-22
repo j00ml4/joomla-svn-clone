61583f49b274f8cbd9b2dc745ff1aa698ba2e028
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-22 19:45:54 +0000

    Working commit.

--- development/branches/comments/components/com_content/controllers/social.php
+++ development/branches/comments/components/com_content/controllers/social.php
@@ -13,7 +13,45 @@ jimport('joomla.application.component.controllersocial');
 /**
  * @package		Joomla.Site
  * @subpackage	com_content
+ * @since		1.6
  */
 class ContentControllerSocial extends JControllerSocial
 {
+	/**
+	 * Method to determine if the user can comment on the selected content.
+	 *
+	 * @param	string	The context for the content.
+	 * @return	boolean
+	 * @since	1.6
+	 */
+	protected function canComment($context)
+	{
+		// The context should be in the form com_content.article.N
+		// where N is the article id.
+		$parts = explode('.', $context);
+
+		if (count($parts) != 3) {
+			return false;
+		}
+
+		if ($parts[0] != 'com_content') {
+			return false;
+		}
+
+		if ($parts[1] != 'article') {
+			return false;
+		}
+
+		$id = (int) $parts[2];
+		if (empty($id)) {
+			return false;
+		}
+
+		$model = JModel::getInstance('Article', 'ContentModel', array('ignore_request' => true));
+		if ($article = $model->getItem($id)) {
+			return $article->params->get('article-allow_comments');
+		} else {
+			return false;
+		}
+	}
 }
