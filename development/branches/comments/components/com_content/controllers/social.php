5954042e683e9f2de97e380ed861405587e7ef3f
eddieajau <eddieajau@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-22 19:15:07 +0000

    Working commit.

---
+++ development/branches/comments/components/com_content/controllers/social.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version		$Id$
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.controllersocial');
+
+/**
+ * @package		Joomla.Site
+ * @subpackage	com_content
+ */
+class ContentControllerSocial extends JControllerSocial
+{
+}
