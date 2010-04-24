9e83b6ad862c6822699f7fb1380ec8f567e86301
chdemko <chdemko@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-24 07:13:53 +0000

    Merge from trunk 16378:16411

--- development/branches/langswitcher/administrator/components/com_banners/models/banners.php
+++ development/branches/langswitcher/administrator/components/com_banners/models/banners.php
@@ -1,12 +1,11 @@
 <?php
+
 /**
  * @version		$Id$
  * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die;
-
 jimport('joomla.application.component.modellist');
 
 /**
@@ -16,120 +15,47 @@ jimport('joomla.application.component.modellist');
  * @subpackage	com_banners
  * @since		1.6
  */
-class BannersModelBanners extends JModelList
-{
-	/**
-	 * Categories data
-	 * @var		array
-	 */
-	protected $categories;
+class BannersModelBanners extends JModelList {
 
 	/**
-	 * Method to auto-populate the model state.
-	 *
-	 * Note. Calling getState in this method will result in recursion.
+	 * Method to get the maximum ordering value for each category.
 	 *
 	 * @since	1.6
 	 */
-	protected function populateState()
-	{
-		// Initialise variables.
-		$app = JFactory::getApplication('administrator');
-
-		// Load the filter state.
-		$data = JRequest::getVar('filters');
-		if (empty($data)) {
-			$data = $app->getUserState($this->context.'.data');
+	function &getCategoryOrders() {
+		if (!isset($this->cache['categoryorders'])) {
+			$db = $this->getDbo();
+			$query = $db->getQuery(true);
+			$query->select('MAX(ordering) as `max`, catid');
+			$query->select('catid');
+			$query->from('#__banners');
+			$query->group('catid');
+			$db->setQuery($query);
+			$this->cache['categoryorders'] = $db->loadAssocList('catid', 0);
 		}
-		else {
-			$app->setUserState($this->context.'.data', $data);
-		}
-		$this->setState('filter.search', isset($data['search']['expr']) ? $data['search']['expr'] : '');
-
-		$this->setState('filter.published', isset($data['select']['state']) ? $data['select']['state'] : '');
-		$this->setState('filter.access', isset($data['select']['access']) ? $data['select']['access'] : '');
-		$this->setState('filter.client_id', isset($data['select']['client']) ? $data['select']['client'] : '');
-		$this->setState('filter.category_id', isset($data['select']['category']) ? $data['select']['category'] : '');
-		$this->setState('filter.language', isset($data['select']['language']) ? $data['select']['language'] : '');
-
-		// Load the parameters.
-		$params = JComponentHelper::getParams('com_banners');
-		$this->setState('params', $params);
-
-		// List state information.
-		parent::populateState('name', 'asc');
-	}
-
-	/**
-	 * Returns a reference to the a Table object, always creating it.
-	 *
-	 * @param	type	The table type to instantiate
-	 * @param	string	A prefix for the table class name. Optional.
-	 * @param	array	Configuration array for model. Optional.
-	 * @return	JTable	A database object
-	*/
-	public function getTable($type = 'Banner', $prefix = 'BannersTable', $config = array())
-	{
-		return JTable::getInstance($type, $prefix, $config);
-	}
-
-	/**
-	 * Method to get a store id based on model configuration state.
-	 *
-	 * This is necessary because the model is used by the component and
-	 * different modules that might need different sets of data or different
-	 * ordering requirements.
-	 *
-	 * @param	string		$id	A prefix for the store id.
-	 *
-	 * @return	string		A store id.
-	 */
-	protected function getStoreId($id = '')
-	{
-		// Compile the store id.
-		$id	.= ':'.$this->getState('filter.search');
-		$id	.= ':'.$this->getState('filter.access');
-		$id	.= ':'.$this->getState('filter.published');
-		$id	.= ':'.$this->getState('filter.category_id');
-		$id	.= ':'.$this->getState('filter.language');
-
-		return parent::getStoreId($id);
+		return $this->cache['categoryorders'];
 	}
 
 	/**
 	 * Build an SQL query to load the list data.
 	 *
 	 * @return	JDatabaseQuery
+	 * @since	1.6
 	 */
-	protected function getListQuery()
-	{
-		// Get the application object
-		$app = &JFactory::getApplication();
+	protected function getListQuery() {
 
-		// Create a new query object.
+		// Initialise variables.
 		$db = $this->getDbo();
 		$query = $db->getQuery(true);
 
 		// Select the required fields from the table.
-		$query->select(
-			$this->getState(
-				'list.select',
-				'a.id AS id, a.name AS name, a.alias AS alias,'.
-				'a.checked_out AS checked_out,'.
-				'a.checked_out_time AS checked_out_time, a.catid AS catid,' .
-				'a.clicks AS clicks, a.metakey AS metakey, a.sticky AS sticky,'.
-				'a.impmade AS impmade, a.imptotal AS imptotal,' .
-				'a.state AS state, a.ordering AS ordering,'.
-				'a.purchase_type as purchase_type,'.
-				'a.language'
-			)
-		);
+		$query->select($this->getState('list.select', 'a.id AS id, a.name AS name, a.alias AS alias,' . 'a.checked_out AS checked_out,' . 'a.checked_out_time AS checked_out_time, a.catid AS catid,' . 'a.clicks AS clicks, a.metakey AS metakey, a.sticky AS sticky,' . 'a.impmade AS impmade, a.imptotal AS imptotal,' . 'a.state AS state, a.ordering AS ordering,' . 'a.purchase_type as purchase_type,' . 'a.language'));
 		$query->from('`#__banners` AS a');
 
 		// Join over the language
 		$query->select('l.title AS language_title');
 		$query->join('LEFT', '`#__languages` AS l ON l.lang_code = a.language');
-		
+
 		// Join over the users for the checked out user.
 		$query->select('uc.name AS editor');
 		$query->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
@@ -145,7 +71,7 @@ class BannersModelBanners extends JModelList
 		// Filter by published state
 		$published = $this->getState('filter.published');
 		if (is_numeric($published)) {
-			$query->where('a.state = '.(int) $published);
+			$query->where('a.state = ' . (int)$published);
 		}
 		else if ($published === '') {
 			$query->where('(a.state IN (0, 1))');
@@ -154,67 +80,66 @@ class BannersModelBanners extends JModelList
 		// Filter by category.
 		$categoryId = $this->getState('filter.category_id');
 		if (is_numeric($categoryId)) {
-			$query->where('a.catid = '.(int) $categoryId);
+			$query->where('a.catid = ' . (int)$categoryId);
 		}
 
 		// Filter by client.
 		$clientId = $this->getState('filter.client_id');
 		if (is_numeric($clientId)) {
-			$query->where('a.cid = '.(int) $clientId);
+			$query->where('a.cid = ' . (int)$clientId);
 		}
 
 		// Filter by search in title
 		$search = $this->getState('filter.search');
 		if (!empty($search)) {
 			if (stripos($search, 'id:') === 0) {
-				$query->where('a.id = '.(int) substr($search, 3));
+				$query->where('a.id = ' . (int)substr($search, 3));
 			}
-			else
-			{
-				$search = $db->Quote('%'.$db->getEscaped($search, true).'%');
-				$query->where('(a.name LIKE '.$search.' OR a.alias LIKE '.$search.')');
+			else {
+				$search = $db->Quote('%' . $db->getEscaped($search, true) . '%');
+				$query->where('(a.name LIKE ' . $search . ' OR a.alias LIKE ' . $search . ')');
 			}
 		}
 
 		// Filter on the language.
 		if ($language = $this->getState('filter.language')) {
-			$query->where('a.language = '.$db->quote($language == '?' ? '' : $language));
+			$query->where('a.language = ' . $db->quote($language == '?' ? '' : $language));
 		}
 
 		// Add the list ordering clause.
-		$orderCol = $this->getState('list.ordering', 'ordering');
-		$app->setUserState($this->context . '.'.$orderCol.'.orderdirn',$this->getState('list.direction', 'ASC'));
-		if ($orderCol=='ordering') {
-			$query->order($db->getEscaped('category_title').' '.$db->getEscaped($app->getUserState($this->context . '.category_title.orderdirn','ASC')));
+		$orderCol = $this->state->get('list.ordering');
+		$orderDirn = $this->state->get('list.direction');
+		if ($orderCol == 'ordering' || $orderCol == 'category_title') {
+			$orderCol = 'category_title ' . $orderDirn . ', ordering';
 		}
-		$query->order($db->getEscaped($orderCol).' '.$db->getEscaped($this->getState('list.direction', 'ASC')));
-		if ($orderCol=='category_title') {
-			$query->order($db->getEscaped('ordering').' '.$db->getEscaped($app->getUserState($this->context . '.ordering.orderdirn','ASC')));
-		}
-		$query->order($db->getEscaped('state').' '.$db->getEscaped($app->getUserState($this->context . '.published.orderdirn','ASC')));
+		$query->order($db->getEscaped($orderCol . ' ' . $orderDirn));
 
 		//echo nl2br(str_replace('#__','jos_',$query));
 		return $query;
 	}
+
 	/**
-	 * method to give information about categories
+	 * Method to get a store id based on model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param	string		$id	A prefix for the store id.
+	 * @return	string		A store id.
+	 * @since	1.6
 	 */
-	function &getCategories()
-	{
-		if (!isset($this->categories))
-		{
-			$db = $this->getDbo();
-			$query = $db->getQuery(true);
-			$query->select('MAX(ordering) as `max`');
-			$query->select('catid');
-			$query->from('#__banners');
-			$query->where('state>=0');
-			$query->group('catid');
-			$db->setQuery((string)$query);
-			$this->categories = $db->loadObjectList('catid');
-		}
-		return $this->categories;
+	protected function getStoreId($id = '') {
+
+		// Compile the store id.
+		$id.= ':' . $this->getState('filter.search');
+		$id.= ':' . $this->getState('filter.access');
+		$id.= ':' . $this->getState('filter.published');
+		$id.= ':' . $this->getState('filter.category_id');
+		$id.= ':' . $this->getState('filter.language');
+		return parent::getStoreId($id);
 	}
+
 	/**
 	 * Method to get the row form.
 	 *
@@ -238,11 +163,61 @@ class BannersModelBanners extends JModelList
 		}
 
 		// Check the session for previously entered form data.
-		$data = $app->getUserState($this->context.'.data', array());
+		$data = $app->getUserState($this->context . '.data', array());
+
 		// Bind the form data if present.
 		if (!empty($data)) {
 			$form->bind($data);
 		}
 		return $form;
 	}
+
+	/**
+	 * Returns a reference to the a Table object, always creating it.
+	 *
+	 * @param	type	The table type to instantiate
+	 * @param	string	A prefix for the table class name. Optional.
+	 * @param	array	Configuration array for model. Optional.
+	 * @return	JTable	A database object
+	 * @since	1.6
+	 */
+	public function getTable($type = 'Banner', $prefix = 'BannersTable', $config = array()) {
+		return JTable::getInstance($type, $prefix, $config);
+	}
+
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @since	1.6
+	 */
+	protected function populateState() {
+
+		// Initialise variables.
+		$app = JFactory::getApplication('administrator');
+
+		// Load the filter state.
+		$data = JRequest::getVar('filters');
+		if (empty($data)) {
+			$data = $app->getUserState($this->context . '.data');
+		}
+		else {
+			$app->setUserState($this->context . '.data', $data);
+		}
+		$this->setState('filter.search', isset($data['search']['expr']) ? $data['search']['expr'] : '');
+		$this->setState('filter.published', isset($data['select']['state']) ? $data['select']['state'] : '');
+		$this->setState('filter.access', isset($data['select']['access']) ? $data['select']['access'] : '');
+		$this->setState('filter.client_id', isset($data['select']['client']) ? $data['select']['client'] : '');
+		$this->setState('filter.category_id', isset($data['select']['category']) ? $data['select']['category'] : '');
+		$this->setState('filter.language', isset($data['select']['language']) ? $data['select']['language'] : '');
+
+		// Load the parameters.
+		$params = JComponentHelper::getParams('com_banners');
+		$this->setState('params', $params);
+
+		// List state information.
+		parent::populateState('name', 'asc');
+	}
 }
+
