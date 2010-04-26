4cce629018eb388d4b98096558fea0e34724386b
dextercowley <dextercowley@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-26 14:57:19 +0000

    # [#20238] Updating help sites list

--- development/releases/1.5/CHANGELOG.php
+++ development/releases/1.5/CHANGELOG.php
@@ -39,6 +39,7 @@ $ -> Language fix or change
 26-Apr-2010 Mark Dexter
  # [#20219] Upgrade to 1.5.16 breaks PHP 4 compatibility
  # [#20239] Update ru-RU installation language
+ # [#20238] Updating help sites list
 
 25-Apr-2010 Ian MacLennan
  # [#20221] Can't Login When Session Handler is None
