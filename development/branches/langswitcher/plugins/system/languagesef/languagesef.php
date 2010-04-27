5ecaa290f23d5c85e13333c00aac46650271fc58
infograf768 <infograf768@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-27 04:49:55 +0000

    Reducing queries a lot and enbling cache per language (Thanks Dennis!)

--- development/branches/langswitcher/plugins/system/languagesef/languagesef.php
+++ development/branches/langswitcher/plugins/system/languagesef/languagesef.php
@@ -19,88 +19,85 @@ jimport('joomla.plugin.plugin');
  */
 class plgSystemLanguageSEF extends JPlugin
 {
+	public static $languages, $default_language;
+	
 	public function onAfterInitialise()
 	{
 		$app = JFactory::getApplication();
 		if ($app->isSite()) {
-			$router = $app->getRouter();
+			$router =& $app->getRouter();
 
 			// attach build rules for language SEF
-			$router->attachBuildRule(array($this, "buildRule"));
+			$router->attachBuildRule(array($this, 'buildRule'));
 
 			// attach parse rules for language SEF
-			$router->attachParseRule(array($this, "parseRule"));
+			$router->attachParseRule(array($this, 'parseRule'));
+			
+			// load languages
+			$db 	=& JFactory::getDBO();
+			$query	= $db->getQuery(true);
+			$query->select('*')->from('#__languages')->where('published=1');
+			$db->setQuery($query);
+			$this->languages = $db->loadObjectList('lang_code');
+			
+			$this->default_language = JComponentHelper::getParams('com_languages')->get('site');
 		}
 	}
+	
 	public function buildRule(&$router, &$uri)
 	{
-		if ($router->getMode()==JROUTER_MODE_SEF) {
+		if ($router->getMode() == JROUTER_MODE_SEF) {
 			$language	= $uri->getVar('language');
+			$language   = $language ? $language : JFactory::getLanguage()->getTag();
 			$Itemid		= $uri->getVar('Itemid');
-			$db			= JFactory::getDBO();
-			$query		= $db->getQuery(true);
-			$query->from('#__languages');
-			$query->select('sef');
-			$query->select('lang_code');
-			$query->where('lang_code='.$db->quote($language ? $language : JFactory::getLanguage()->getTag()));
-			$query->join('LEFT','#__menu as m on m.id='.$db->quote($Itemid));
-			$query->select('m.home');
-			$db->setQuery($query);
-			$result = $db->loadObject();
-			if ($result && $result->sef) {
+
+			if (isset($this->languages[$language]) && $this->languages[$language]->sef) {
 				$uri->delVar('language');
-				$path = $uri->getPath();
-				if ($result->home && $language) {
-					$uri->delVar('option');
-					$uri->delVar('Itemid');
+				if ($Itemid) {
+					$menu =& JSite::getMenu()->getItem($Itemid);
+					// if no menu - that means that we are routing home menu item of none-current language
+					if (!$menu || $menu->home) {
+						$uri->delVar('option');
+						$uri->delVar('Itemid');
+					}
 				}
-				$component = JComponentHelper::getParams('com_languages');
-				if ($result->lang_code != $component->get('site') || $this->params->get('default_prefix')) {
-					$uri->setPath($path.'/'.$result->sef.'/');
+				if ($this->languages[$language]->lang_code != $this->default_language || $this->params->get('default_prefix')) {
+					$uri->setPath($uri->getPath().'/'.$this->languages[$language]->sef.'/');
 				}
 			}
 		}
 	}
+	
 	public function parseRule(&$router, &$uri)
 	{
 		$array = array();
-		if ($router->getMode()==JROUTER_MODE_SEF) {
+		if ($router->getMode() == JROUTER_MODE_SEF) {
 			$path = $uri->getPath();
-			$parts = explode('/',$path);
-			$db = JFactory::getDBO();
-			$query = $db->getQuery(true);
-			
-			// Try to find the language according to the sef prefix
-			$query->from($db->nameQuote('#__languages'));
-			$query->select('sef');
-			$query->select('lang_code');
-			$query->where('sef='.$db->quote($parts[0]));
-			$query->where('published=1');
-			$db->setQuery($query);
-			$result = $db->loadObject();
-			if (!$result ||  !JLanguage::exists($result->lang_code)) {
+			$parts = explode('/', $path);
 
+			$lang_code = null;
+			foreach($this->languages as $language) {
+				if ($language->sef == $parts[0]) {
+					$lang_code = $language->lang_code;
+					break; 
+				}
+			}
+			
+			if (!$lang_code ||  !JLanguage::exists($lang_code)) {
 				// Use the default language
-				$component = JComponentHelper::getParams('com_languages');
-				$query = $db->getQuery(true);
-				$query->from($db->nameQuote('#__languages'));
-				$query->select('sef');
-				$query->select('lang_code');
-				$query->where('lang_code='.$db->quote($component->get('site')));
-				$db->setQuery($query);
-				$result = $db->loadObject();
+				$lang_code = $this->default_language;
 			}
 			else {
 				array_shift($parts);
-				$uri->setPath(implode('/',$parts));
+				$uri->setPath(implode('/', $parts));
 			}
-			$array = array('language'=>$result->lang_code);
-			$lang = JFactory::getLanguage();
-			$lang->setLanguage($result->lang_code);
+
+			$array = array('language' => $lang_code);
+			JFactory::getLanguage()->setLanguage($lang_code);
 			$config = JFactory::getConfig();
-			$cookie_domain = $config->get('config.cookie_domain', '');
-			$cookie_path = $config->get('config.cookie_path', '/');
-			setcookie(JUtility::getHash('language'), $result->lang_code, time() + 365 * 86400, $cookie_path, $cookie_domain);
+			$cookie_domain 	= $config->get('config.cookie_domain', '');
+			$cookie_path 	= $config->get('config.cookie_path', '/');
+			setcookie(JUtility::getHash('language'), $lang_code, time() + 365 * 86400, $cookie_path, $cookie_domain);
 		}
 		return $array;
 	}
