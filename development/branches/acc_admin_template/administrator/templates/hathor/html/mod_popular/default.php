ef5b4d1a3af8fa63650bf1af55b215dc6905d82d
andrea.tarr <andrea.tarr@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-29 11:41:44 +0000

    Fix the beginning doc blocks

--- development/branches/acc_admin_template/administrator/templates/hathor/html/mod_popular/default.php
+++ development/branches/acc_admin_template/administrator/templates/hathor/html/mod_popular/default.php
@@ -2,9 +2,10 @@
 /**
  * @version		$Id$
  * @package		Joomla.Administrator
- * @subpackage	mod_popular
+ * @subpackage	templates.hathor
  * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @since		1.6
  */
 
 // no direct access
