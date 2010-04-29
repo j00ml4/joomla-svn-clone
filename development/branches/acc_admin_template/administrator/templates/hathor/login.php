6fd3415363372e119023dab20dd7ccf2b8c3e54a
andrea.tarr <andrea.tarr@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-04-29 11:52:34 +0000

    fix login doc block

--- development/branches/acc_admin_template/administrator/templates/hathor/login.php
+++ development/branches/acc_admin_template/administrator/templates/hathor/login.php
@@ -6,6 +6,7 @@
  * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  * @since		1.6
+ */
 
 // no direct access
 defined('_JEXEC') or die;
