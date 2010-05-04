122feb39f6f5ce5b3b73da52084b3a6e201b607f
wonderslug <wonderslug@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-05-04 10:13:33 +0000

    adding gantry lib

---
+++ development/branches/rocket/libraries/gantry/ajax-models/example.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package     gantry
+ * @subpackage  ajax-models
+ * @version		${project.version} ${build_date}
+ * @author		RocketTheme http://www.rockettheme.com
+ * @copyright 	Copyright (C) 2007 - ${copyright_year} RocketTheme, LLC
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ *
+ * Gantry uses the Joomla Framework (http://www.joomla.org), a GNU/GPLv2 content management system
+ *
+ */
+defined('JPATH_BASE') or die();
+global $gantry;
+
+print_r(JRequest::get());
+
+?>
