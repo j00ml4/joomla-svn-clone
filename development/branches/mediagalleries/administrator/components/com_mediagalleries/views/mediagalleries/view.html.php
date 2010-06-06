62f087603ca3bc4e4160fba4fb93cdfeab74a139
amangautam <amangautam@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-06-06 18:46:25 +0000

    added backend of com_mediagalleries

---
+++ development/branches/mediagalleries/administrator/components/com_mediagalleries/views/mediagalleries/view.html.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * mediagalleries View 
+ * 
+ * @package    Joomla.Tutorials
+ * @subpackage Components
+ * @link http://dev.joomla.org/component/option,com_jd-wiki/Itemid,31/id,tutorials:components/
+ * @license        GNU/GPL
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+
+jimport( 'joomla.application.component.view' );
+
+/**
+ * mediagalleries View
+ *
+ * @package    Joomla.Tutorials
+ * @subpackage Components
+ */
+class mediagalleriesViewmediagalleries extends JView
+{
+    /**
+     * Hellos view display method
+     * @return void
+     **/
+    function display($tpl = null)
+    {
+    	
+    	 global  $option;
+    	$mainframe = &JFactory::getApplication();
+		//print_r($mainframe);
+		// Define
+		$db		=& JFactory::getDBO();
+		$uri	=& JFactory::getURI();
+		
+		
+		$this->state	= $this->get('State');
+		$filter_state		= $mainframe->getUserStateFromRequest( $option.'filter_state', 'filter_state', '', 'word' );
+		$filter_catid		= $mainframe->getUserStateFromRequest( $option.'filter_catid', 'catid', 0, 'int' );
+		$filter_order		= $mainframe->getUserStateFromRequest( $option.'filter_order',		'filter_order',		'a.ordering',	'cmd' );
+		$filter_order_Dir	= $mainframe->getUserStateFromRequest( $option.'filter_order_Dir',	'filter_order_Dir',	'',				'word' );
+		$search				= $mainframe->getUserStateFromRequest( $option.'search',			'search',			'',				'string' );
+		$search				= JString::strtolower( $search );
+	
+		// Get data from the model
+		$items		= & $this->get( 'Data');
+		$total		= & $this->get( 'Total');
+		$pagination = & $this->get( 'Pagination' );
+
+		// build list of categories
+		$javascript 	= 'onchange="document.adminForm.submit();"';
+		$lists['catid'] = JHTML::_('list.category',  'catid', $option, intval( $filter_catid ), $javascript );
+		$lists['state']	= JHTML::_('grid.state',  $filter_state );
+		
+		// table ordering
+		$lists['order_Dir'] = $filter_order_Dir;
+		$lists['order'] = $filter_order;
+
+		// search filter
+		$lists['search']= $search;
+
+		$action = JRoute::_('index.php?option=com_mediagalleries');
+		$this->assignRef('action', 	$action);		
+		
+		// Assign References
+		$this->assignRef('user',		JFactory::getUser());
+		$this->assignRef('lists',		$lists);
+		$this->assignRef('items',		$items);
+		$this->assignRef('pagination',	$pagination);
+		
+
+		// Display
+		parent::display($tpl);
+		
+    }
+	
+}
