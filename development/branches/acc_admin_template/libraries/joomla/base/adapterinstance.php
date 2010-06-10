1cb663e62ca709505b3e63d12e835a3e4d039fcc
andrea.tarr <andrea.tarr@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-06-10 02:16:52 +0000

    Merging r17502 through r17589

--- development/branches/acc_admin_template/libraries/joomla/base/adapterinstance.php
+++ development/branches/acc_admin_template/libraries/joomla/base/adapterinstance.php
@@ -27,17 +27,21 @@ class JAdapterInstance extends JObject {
 	 * @var object */
 	protected $db = null;
 
-
 	/**
 	 * Constructor
 	 *
 	 * @access	public
-	 * @param	object	$parent	Parent object [JAdapter instance]
+	 * @param	object	$parent		Parent object [JAdapter instance]
+	 * @param	object	$db			Database object [JDatabase instance]
+	 * @param 	array	$options	Configuration Options
 	 * @return	void
 	 * @since	1.6
 	 */
-	public function __construct(&$parent, &$db)
+	public function __construct(&$parent, &$db, $options = Array())
 	{
+		// set the properties from the options array that is passed in
+		$this->setProperties($options); 
+		// set the parent and db in case $options for some reason overrides it
 		$this->parent =& $parent;
 		$this->db =& $db ? $db : JFactory::getDBO(); // pull in the global dbo in case
 	}
