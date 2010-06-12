7645ab3b861603861011e1bdb3d2de46bf376dd1
andrea.tarr <andrea.tarr@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-06-12 16:03:00 +0000

    Merging r17589 through r17625

--- development/branches/acc_admin_template/administrator/templates/hathor/html/com_content/articles/modal.php
+++ development/branches/acc_admin_template/administrator/templates/hathor/html/com_content/articles/modal.php
@@ -58,12 +58,9 @@ $lang->load('tpl_hathor', JPATH_ADMINISTRATOR)
 		</div>
 	</fieldset>
 
-	<table class="adminlist">
+	<table class="adminlist modal">
 		<thead>
 			<tr>
-				<th class="checkmark-col">
-					<input type="checkbox" name="toggle" value="" title="<?php echo JText::_('TPL_HATHOR_CHECKMARK_ALL'); ?> " onclick="checkAll(this)" />
-				</th>
 				<th class="title">
 					<?php echo JHtml::_('grid.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
 				</th>
@@ -71,7 +68,7 @@ $lang->load('tpl_hathor', JPATH_ADMINISTRATOR)
 					<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_CATEGORY', 'a.catid', $listDirn, $listOrder); ?>
 				</th>
 				<th class="title access-col">
-					<?php echo JHtml::_('grid.sort',  'JGRID_HEADING_ACCESS', 'category', $listDirn, $listOrder); ?>
+					<?php echo JHtml::_('grid.sort',  'JGRID_HEADING_ACCESS', 'access_level', $listDirn, $listOrder); ?>
 				</th>
 				<th class="title date-col">
 					<?php echo JHtml::_('grid.sort',  'JDATE', 'a.created', $listDirn, $listOrder); ?>
@@ -85,13 +82,10 @@ $lang->load('tpl_hathor', JPATH_ADMINISTRATOR)
 		<tbody>
 		<?php foreach ($this->items as $i => $item) : ?>
 			<tr class="row<?php echo $i % 2; ?>">
-				<td class="center">
-					<?php echo JHtml::_('grid.id', $i, $item->id); ?>
-				</td>
-				<td>
-					<a class="pointer" onclick="if (window.parent) window.parent.<?php echo $function;?>('<?php echo $item->id; ?>', '<?php echo $this->escape($item->title); ?>', '<?php echo $this->escape($item->catid); ?>');">
+				<th>
+					<a class="pointer" onclick="if (window.parent) window.parent.<?php echo $function;?>('<?php echo $item->id; ?>', '<?php echo $this->escape(addslashes($item->title)); ?>', '<?php echo $this->escape($item->catid); ?>');">
 						<?php echo $this->escape($item->title); ?></a>
-				</td>
+				</th>
 				<td class="center">
 					<?php echo $this->escape($item->category_title); ?>
 				</td>
--- development/branches/acc_admin_template/administrator/templates/hathor/html/com_users/users/modal.php
+++ development/branches/acc_admin_template/administrator/templates/hathor/html/com_users/users/modal.php
@@ -22,6 +22,7 @@ $function = 'jSelectUser_'.JRequest::getVar('field');
 			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->state->get('filter.search'); ?>" title="<?php echo JText::_('COM_USERS_SEARCH_IN_NAME'); ?>" />
 			<button type="submit"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
 			<button type="button" onclick="document.id('filter_search').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
+			<button type="button" onclick="if (window.parent) window.parent.<?php echo $function;?>('', '<?php echo JText::_('JLIB_FORM_SELECT_USER') ?>');"><?php echo JText::_('JOPTION_NO_USER')?></button>
 		</div>
 		<div class="filter-select">
 			<label for="filter_group_id">
@@ -35,7 +36,7 @@ $function = 'jSelectUser_'.JRequest::getVar('field');
 		</div>
 	</fieldset>
 
-	<table class="adminlist">
+	<table class="adminlist modal">
 		<thead>
 			<tr>
 				<th class="title">
@@ -56,7 +57,7 @@ $function = 'jSelectUser_'.JRequest::getVar('field');
 			foreach ($this->items as $item) : ?>
 			<tr class="row<?php echo $i % 2; ?>">
 				<td>
-					<a class="pointer" onclick="if (window.parent) window.parent.<?php echo $function;?>('<?php echo $item->id; ?>', '<?php echo $this->escape($item->name); ?>');">
+					<a class="pointer" onclick="if (window.parent) window.parent.<?php echo $function;?>('<?php echo $item->id; ?>', '<?php echo $this->escape(addslashes($item->name)); ?>');">
 						<?php echo $item->name; ?></a>
 				</td>
 				<td class="center">
