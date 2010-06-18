4d240872ced1a024ee1a2d323186c6923a9192a1
amangautam <amangautam@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-06-18 16:37:51 +0000

    sql files commited

--- development/branches/mediagalleries/administrator/components/com_mediagalleries/sql/update.mysql.sql
+++ development/branches/mediagalleries/administrator/components/com_mediagalleries/sql/update.mysql.sql
@@ -1,13 +1,6 @@
-
-ALTER TABLE `#__mediagalleries` 
-ADD `created` datetime NOT NULL,
-ADD `created_by` int(10) unsigned NOT NULL,
-ADD `created_by_alias` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-ADD `modified` datetime NOT NULL,
-ADD `modified_by` int(10) unsigned NOT NULL,
-ADD `state` tinyint(1) NOT NULL DEFAULT '1',
-ADD `language` char(7) COLLATE utf8_unicode_ci NOT NULL,
-ADD `featured` tinyint(3) unsigned NOT NULL,
-ADD `xreference` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-ADD `params` text COLLATE utf8_unicode_ci NOT NULL,
-ADD `access` int(10) unsigned NOT NULL;
+ALTER TABLE `#__mediagalleries`
+  DROP `userid`,
+  DROP `media`,
+  DROP `rank`,
+  DROP `votes`,
+  DROP `added`;
