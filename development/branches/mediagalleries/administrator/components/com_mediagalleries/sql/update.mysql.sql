af71df48dee915d1d972417b632098619e17bca6
amangautam <amangautam@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-07-02 15:56:20 +0000

    update.mysql.sql

--- development/branches/mediagalleries/administrator/components/com_mediagalleries/sql/update.mysql.sql
+++ development/branches/mediagalleries/administrator/components/com_mediagalleries/sql/update.mysql.sql
@@ -1,14 +1,3 @@
-ALTER TABLE `jos_mediagalleries` CHANGE `url` `url` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-CHANGE `media` `media` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-CHANGE `thumb_url` `thumb_url` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-CHANGE `title` `title` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-CHANGE `alias` `alias` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-CHANGE `description` `description` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-CHANGE `created_by_alias` `created_by_alias` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-CHANGE `language` `language` CHAR( 7 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-
-ALTER TABLE `jos_mediagalleries_channels` CHANGE `description` `description` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-CHANGE `title` `title` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL 
-
-ALTER TABLE `jos_mediagalleries_channels_media_type` CHANGE `title` `title` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-CHANGE `description` `description` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL 
+Alter table #__mediagalleries
+ADD `publish_up` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+ADD  `publish_down` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
