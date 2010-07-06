0b007da0dad0c036d4441414e6ba7b5402a8bad9
a.radtke <a.radtke@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-07-06 11:37:27 +0000

    made a lot of CSS changes adapt compoment.php add error.php

--- development/branches/beez/templates/beez_20/component.php
+++ development/branches/beez/templates/beez_20/component.php
@@ -17,19 +17,22 @@ $color = $this->params->get('templatecolor');
 <head>
 	<jdoc:include type="head" />
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/template.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/position.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/layout.css" type="text/css" media="screen,projection" />
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/print.css" type="text/css" media="Print" />
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/<?php echo $color; ?>.css" type="text/css" />
 
 
 <?php if($this->direction == 'rtl') : ?>
-	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/template_rtl.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez/css/template_rtl.css" type="text/css" />
 <?php endif; ?>
 </head>
 <body class="contentpane">
 	<div id="all">
+	<div id="main">
 		<jdoc:include type="message" />
 		<jdoc:include type="component" />
+		</div>
 	</div>
 </body>
 </html>
