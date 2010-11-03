6a89520b98e74327f6ec925c8f17c4ad7951e4aa
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-11-03 14:52:38 +0000

    [#21538] Empty menu item if menu type contains space or uppercase letter

--- development/releases/1.5/libraries/joomla/filter/filterinput.php
+++ development/releases/1.5/libraries/joomla/filter/filterinput.php
@@ -175,9 +175,9 @@ class JFilterInput extends JObject
 				$lang = &JFactory::getLanguage();
 				$result = $lang->transliterate($result);
 				$result = (string) preg_replace(
-					array('/\s+/','/[^A-Za-z0-9\-\_]/'), array('-',''), $result
+					array('/\s+/','/[^A-Za-z0-9\-\_ ]/'), array('-',''), $result
 				);
-				$result = strtolower(trim($result));
+				$result = trim($result);
 				break;
 
 
