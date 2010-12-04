3dbdd9f8100bdef30921091e432ef5324af7805e
louis <louis@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2010-12-04 12:31:41 +0000

    Reworking the updater package to be cleaner and the main XML spec to be ATOM based.

--- development/branches/updater/libraries/joomla/updater/updater.php
+++ development/branches/updater/libraries/joomla/updater/updater.php
@@ -1,171 +1,203 @@
 <?php
 /**
- * Joomla! Update System
- *
- * @version		$Id$
- * @package		Joomla.Framework
- * @subpackage	Updater
- * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License, see LICENSE.php
+ * @version     $Id$
+ * @package     Joomla.Framework
+ * @subpackage  Updater
+ * @copyright   Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// Check to ensure this file is within the rest of the framework
-defined('JPATH_BASE') or die();
+defined('JPATH_BASE') or die;
 
-jimport('joomla.filesystem.file');
-jimport('joomla.filesystem.folder');
-jimport('joomla.filesystem.archive');
-jimport('joomla.filesystem.path');
 jimport('joomla.base.adapter');
 
 /**
- * Updater Class
- * @since 1.6
+ * Updater Class for the Joomla Framework.
+ *
+ * @package     Joomla.Framework
+ * @subpackage  Updater
+ * @since       1.6
  */
-class JUpdater extends JAdapter {
+class JUpdater extends JAdapter
+{
+	/**
+	 * @var  JUpdater  The global updater object.
+	 */
+	protected static $instance;
 
 	/**
-	 * Constructor
+	 * Method to instantiate the updater object.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.6
 	 */
-	public function __construct() {
-		// adapter base path, class prefix
+	public function __construct()
+	{
+		// Set the adapter base path and class prefix.
 		parent::__construct(dirname(__FILE__),'JUpdater');
 	}
 
 	/**
-	 * Returns a reference to the global Installer object, only creating it
-	 * if it doesn't already exist.
+	 * Multi-dimensional array safe method to get only unique values for an array.
 	 *
-	 * @static
-	 * @return	object	An installer object
+	 * @param   array  Input array.
+	 *
+	 * @return  array  Output array with only unique values from the input.
+	 *
+	 * @see     http://php.net/manual/en/function.array-unique.php
+	 * @since   1.6
 	 */
-	public function &getInstance()
+	public static function arrayUnique($arr)
 	{
-		static $instance;
-
-		if (!isset ($instance)) {
-			$instance = new JUpdater();
+		// If not an array just return.
+		if (!is_array($arr)) {
+			return $arr;
 		}
-		return $instance;
+
+		// Serialize all array values.
+		$arr = array_map('serialize', $arr);
+
+		// Get the unique elements in a serialized state.
+		$arr = array_unique($arr);
+
+		// Unserialize all array values.
+		$arr = array_map('unserialize', $arr);
+
+		return $arr;
 	}
 
 	/**
-	 * Finds an update for an extension
-	 * @param int Extension Identifier; if zero use all sites
-	 * @return boolean If there are updates or not
+	 * Method to return a reference to the global updater object.  The object is only created if it
+	 * does not already exist.
+	 *
+	 * @return  JUpdater  A reference to the global updater object.
+	 *
+	 * @since   1.6
 	 */
-	public function findUpdates($eid=0) {
-		$dbo = $this->getDBO();
-		$retval = false;
-		// push it into an array
-		if(!is_array($eid)) {
-			$query = 'SELECT DISTINCT update_site_id, type, location FROM #__update_sites WHERE enabled = 1';
-		} else {
-			$query = 'SELECT DISTINCT update_site_id, type, location FROM #__update_sites WHERE update_site_id IN (SELECT update_site_id FROM #__update_sites_extensions WHERE extension_id IN ('. implode(',', $eid) .'))';
+	public static function & getInstance()
+	{
+		if (!isset(self::$instance)) {
+			self::$instance = new JUpdater();
 		}
-		$dbo->setQuery($query);
-		$results = $dbo->loadAssocList();
-		$result_count = count($results);
-		for($i = 0; $i < $result_count; $i++)
-		{
-			$result = &$results[$i];
-			$this->setAdapter($result['type']);
-			if(!isset($this->_adapters[$result['type']])) {
-				continue; // ignore update sites requiring adapters we don't have installed
-			}
-			$update_result = $this->_adapters[$result['type']]->findUpdate($result);
-			if(is_array($update_result))
-			{
-				if(array_key_exists('update_sites',$update_result) && count($update_result['update_sites']))
-				{
-					$results = $this->arrayUnique(array_merge($results, $update_result['update_sites']));
-					$result_count = count($results);
-				}
-				if(array_key_exists('updates', $update_result) && count($update_result['updates']))
-				{
-					for($k = 0; $k < count($update_result['updates']); $k++)
-					{
-						$current_update = &$update_result['updates'][$k];
-						$update = JTable::getInstance('update');
-						$extension = JTable::getInstance('extension');
-						$uid = $update->find(Array('element'=>strtolower($current_update->get('element')),
-								'type'=>strtolower($current_update->get('type')),
-								'client_id'=>strtolower($current_update->get('client_id')),
-								'folder'=>strtolower($current_update->get('folder'))));
 
-						$eid = $extension->find(Array('element'=>strtolower($current_update->get('element')),
-								'type'=>strtolower($current_update->get('type')),
-								'client_id'=>strtolower($current_update->get('client_id')),
-								'folder'=>strtolower($current_update->get('folder'))));
-						if(!$uid)
+		return self::$instance;
+	}
+
+	/**
+	 * Method to find any available updates for a set of extensions.  If no specific extensions are
+	 * given then find updates for all extensions.
+	 *
+	 * @param   array    An array of extension ids for which to find updates.
+	 *
+	 * @return  boolean  True if there are updates to process.
+	 *
+	 * @since   1.6
+	 */
+	public function findUpdates($eid = null)
+	{
+		// Initialize variables.
+		$db = & JFactory::getDBO();
+		$found = false;
+
+		// Get a new query object.
+		$query = $db->getQuery(true);
+		$query->select('DISTINCT update_site_id, type, location');
+		$query->from('#__update_sites');
+
+		// If no values are given get all of them.
+		if (empty($eid)) {
+			$query->where('enabled = 1');
+		}
+		else {
+			// Sanitize the array as integers.
+			JArrayHelper::toInteger((array) $eid);
+
+			// Build the requisite subquery.
+			$subQuery = $db->getQuery(true);
+			$subQuery->select('update_site_id');
+			$subQuery->from('#__update_sites_extensions');
+			$subQuery->where('extension_id IN ('.implode(',',$eid).')');
+
+			// Add the subquery.
+			$query->where('update_site_id IN ('.(string) $subQuery.')');
+		}
+
+		// Get the sites to check from the database.
+		$db->setQuery($query);
+		$sites = $db->loadObjectList();
+
+		foreach ($sites as $site)
+		{
+			// Attempt to set the adapter for the update site type.
+			$this->setAdapter($site->type);
+
+			// Ignore any sites for which we have no installed adapters.
+			if(!isset($this->_adapters[$site->type])) {
+				continue;
+			}
+
+			// Get the available updates from the adapter.
+			$updates = (array) $this->_adapters[$site->type]->findUpdate($site);
+
+			if(!empty($updates))
+			{
+				foreach ($updates as $update)
+				{
+					// Get table objects for update and extension.
+					$uRow = JTable::getInstance('update');
+					$eRow = JTable::getInstance('extension');
+
+					$updateId = $uRow->find(array(
+						'element'   => strtolower($update->get('element')),
+						'type'      => strtolower($update->get('type')),
+						'client_id' => strtolower($update->get('client_id')),
+						'folder'    => strtolower($update->get('folder'))
+					));
+
+					$extensionId = $eRow->find(array(
+						'element'   => strtolower($update->get('element')),
+						'type'      => strtolower($update->get('type')),
+						'client_id' => strtolower($update->get('client_id')),
+						'folder'    => strtolower($update->get('folder'))
+					));
+
+					// If there is no existing update id for the update, check and see if we need to
+					// add one based on installed version, etc.
+					if(!$updateId)
+					{
+						// Check to see if we have an installed extension for this update.
+						if($extensionId)
 						{
-							// set the extension id
-							if($eid)
-							{
-								// we have an installed extension, check the update is actually newer
-								$extension->load($eid);
-								$data = unserialize($extension->manifest_cache);
-								if(version_compare($current_update->version, $data['version'], '>') == 1)
-								{
-									$current_update->extension_id = $eid;
-									$current_update->store();
-								}
-							} else
-							{
-								// a potentially new extension to be installed
-								$current_update->store();
+							// Make sure that the update is newer than the installed version.
+							$eRow->load($extensionId);
+							$data = unserialize($eRow->manifest_cache);
+							if(version_compare($update->version, $data['version'], '>') == 1) {
+								$uRow->extension_id = $extensionId;
+								$uRow->store();
+								$found = true;
 							}
-						} else
-						{
-							$update->load($uid);
-							// if there is an update, check that the version is newer then replaces
-							if(version_compare($current_update->version, $update->version, '>') == 1)
-							{
-								$current_update->store();
-							}
+						}
+						// No extension found for this update, potentially new to be installed.
+						else {
+							$uRow->store();
+							$found = true;
+						}
+					}
+					// An update is already in the queue for this extension.
+					else
+					{
+						// if there is an update, check that the version is newer then replaces
+						$uRow->load($updateId);
+						if(version_compare($update->version, $uRow->version, '>') == 1) {
+							$uRow->store();
+							$found = true;
 						}
 					}
 				}
-				$update_result = true;
-			} else if ($retval) {
-				$update_result = true;
 			}
 		}
-		return $retval;
-	}
 
-	/**
-	 * Multidimensional array safe unique test
-	 * Borrowed from PHP.net
-	 * @url http://au2.php.net/manual/en/function.array-unique.php
-	 */
-	public function arrayUnique($myArray)
-	{
-		if (!is_array($myArray)) {
-			return $myArray;
-		}
-
-		foreach ($myArray as &$myvalue){
-			$myvalue=serialize($myvalue);
-		}
-
-		$myArray=array_unique($myArray);
-
-		foreach ($myArray as &$myvalue){
-			$myvalue=unserialize($myvalue);
-		}
-
-		return $myArray;
-	}
-
-	public function update($id)
-	{
-		$updaterow = JTable::getInstance('update');
-		$updaterow->load($id);
-		$update = new JUpdate();
-		if($update->loadFromXML($updaterow->detailsurl)) {
-			return $update->install();
-		}
-		return false;
+		return $found;
 	}
 }
