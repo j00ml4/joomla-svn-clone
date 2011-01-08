382fcf740533cd3391db4acd64617e3b3cb02c33
dextercowley <dextercowley@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-01-08 19:22:01 +0000

    Catch branch up to trunk through rev 20194

--- development/branches/phpstrict/administrator/components/com_users/models/group.php
+++ development/branches/phpstrict/administrator/components/com_users/models/group.php
@@ -94,14 +94,14 @@ class UsersModelGroup extends JModelAdmin
 	 * @throws	Exception if there is an error in the form event.
 	 * @since	1.6
 	 */
-	protected function preprocessForm(JForm $form, $data, $group = 'user')
+	protected function preprocessForm(JForm $form, $data, $groups = '')
 	{
 		$obj = is_array($data) ? JArrayHelper::toObject($data,'JObject') : $data;
 		if (isset($obj->parent_id) && $obj->parent_id == 0 && $obj->id > 0) {
 			$form->setFieldAttribute('parent_id','type','hidden');
 			$form->setFieldAttribute('parent_id','hidden','true');
 		}
-		parent::preprocessForm($form, $data, $group);
+		parent::preprocessForm($form, $data, 'user');
 	}
 
 	/**
--- development/branches/phpstrict/administrator/components/com_users/models/level.php
+++ development/branches/phpstrict/administrator/components/com_users/models/level.php
@@ -176,9 +176,9 @@ class UsersModelLevel extends JModelAdmin
 	 * @throws	Exception if there is an error in the form event.
 	 * @since	1.6
 	 */
-	protected function preprocessForm(JForm $form, $data, $group = 'user')
+	protected function preprocessForm(JForm $form, $data, $groups = '')
 	{
-		parent::preprocessForm($form, $data, $group);
+		parent::preprocessForm($form, $data, 'user');
 	}
 
 	/**
