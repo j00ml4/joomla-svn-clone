681bdc9740f9a81176910482732362e959165ce1
hooduku <hooduku@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-02-07 19:40:53 +0000

    Joomla 1.6 windows azure modifications

---
+++ development/branches/database/Joomla_1.6_WindowsAzure/administrator/components/com_config/views/application/tmpl/default_cloud_server.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @version		$Id: default_cloud_server.php 17109 2010-05-16 21:03:55Z severdia $
+ * @package		Joomla.Administrator
+ * @subpackage	com_config
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+?>
+<script type="text/javascript">
+window.addEvent('domready', function(){
+	if(document.id('jform_storage_type').get("value")=='local'){
+		document.id('jform_cloud_acc_name').set('class', '');
+		document.id('jform_cloud_access_key').set('class', '');
+		document.id('jform_secret_key').set('class', '');
+		document.id('jform_container').set('class', '');
+	}
+	document.id('jform_storage_type').addEvent('change',function() {
+		if(this.get("value")!='local'){
+			document.id('jform_cloud_acc_name').set('class', 'required');
+			document.id('jform_cloud_access_key').set('class', 'required');
+			document.id('jform_secret_key').set('class', 'required');
+			document.id('jform_container').set('class', 'required');
+		  }else{
+			  document.id('jform_cloud_acc_name').set('class', '');
+			  document.id('jform_cloud_access_key').set('class', '');
+			  document.id('jform_secret_key').set('class', '');
+			  document.id('jform_container').set('class', '');
+		  }
+	});
+});
+</script>
+<div class="width-100">
+<fieldset class="adminform">
+	<legend><?php echo JText::_('COM_CONFIG_CLOUD_SERVER_SETTINGS'); ?></legend>
+	<ul class="adminformlist">
+			<?php
+			foreach ($this->form->getFieldset('cloud_server') as $field):
+			?>
+					<li><?php echo $field->label; ?>
+					<?php echo $field->input; ?></li>
+			<?php
+			endforeach;
+			?>
+		</ul>
+</fieldset>
+</div>
