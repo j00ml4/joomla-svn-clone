d70486588b3b22e1ffae83e9d16d955c585f5df5
realityking <realityking@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-02-08 01:45:50 +0000

    Accidentally removed some code with the last commit

--- development/branches/ajax/libraries/joomla/application/component/controller.php
+++ development/branches/ajax/libraries/joomla/application/component/controller.php
@@ -964,4 +964,60 @@ class JController extends JObject
 
 		return $this;
 	}
+
+	/**
+	 * Method to handle a send a JSON response. The data parameter
+	 * can be a JException object for when an error has occurred or
+	 * a JObject for a good response.
+	 *
+	 * @access	public
+	 * @param	object	JObject on success, JException on failure.
+	 * @return	void
+	 * @since	1.7
+	 */
+	function sendJsonResponse($response)
+	{
+		// Check if we need to send an error code.
+		if (JError::isError($response)) {
+			// Send the appropriate error code response.
+			JResponse::setHeader('status', $response->getCode());
+			JResponse::setHeader('Content-Type', 'application/json; charset=utf-8');
+			JResponse::sendHeaders();
+		}
+
+		// Send the JSON response.
+		echo json_encode(new JJsonResponse($response));
+
+		// Close the application.
+		$app = JFactory::getApplication();
+		$app->close();
+	}
+}
+
+	 /**	 
+  * Joomla Core  JSON Response Class	 
+  *	 
+  * @package             Joomla.Framework	 
+  * @subpackage  Application	 
+  * @since               1.7	 
+  */	 
+ class JJsonResponse	 
+ {	 
+	function __construct($state)	 
+	{	 
+		// The old token is invalid so send a new one.	 
+		$this->token = JUtility::getToken(true);	 
+
+		// Check if we are dealing with an error.	 
+		if (JError::isError($state)) {	 
+			// Prepare the error response.	 
+			$this->error    = true;	 
+			$this->header   = JText::_('LIB_HEADER_ERROR');	 
+			$this->message  = $state->getMessage();	 
+		} else {	 
+			// Prepare the response data.	 
+			$this->error    = false;	 
+			$this->data		= $state;	 
+		}	 
+	}
 }
