c9436cb6631c076e5b949b6aa50355d16dd5838c
rvsjoen <rvsjoen@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-02-13 21:44:02 +0000

    Clean up the code in the validation rules

--- development/branches/contact/components/com_contact/models/rules/contactemail.php
+++ development/branches/contact/components/com_contact/models/rules/contactemail.php
@@ -13,92 +13,23 @@ jimport('joomla.form.formrule');
 
 require_once 'libraries/joomla/form/rules/email.php';
 
-/**
- * Form Rule class for the Joomla Framework.
- *
- * @package		Joomla.Framework
- * @subpackage	Form
- * @since		1.6
- */
 class JFormRuleContactEmail extends JFormRuleEmail
 {
-	/**
-	 * The regular expression to use in testing a form field value.
-	 *
-	 * @var		string
-	 * @since	1.6
-	 */
-	protected $regex = '^[\w.-]+(\+[\w.-]+)*@\w+[\w.-]*?\.\w{2,4}$';
-
-	/**
-	 * Method to test the email address and optionally check for uniqueness.
-	 *
-	 * @param	object	$element	The JXMLElement object representing the <field /> tag for the
-	 * 								form field object.
-	 * @param	mixed	$value		The form field value to validate.
-	 * @param	string	$group		The field name group control value. This acts as as an array
-	 * 								container for the field. For example if the field has name="foo"
-	 * 								and the group value is set to "bar" then the full field name
-	 * 								would end up being "bar[foo]".
-	 * @param	object	$input		An optional JRegistry object with the entire data set to validate
-	 * 								against the entire form.
-	 * @param	object	$form		The form object for which the field is being tested.
-	 *
-	 * @return	boolean	True if the value is valid, false otherwise.
-	 * @since	1.6
-	 * @throws	JException on invalid rule.
-	 */
 	public function test(& $element, $value, $group = null, & $input = null, & $form = null)
 	{
-		// If the field is empty and not required, the field is valid.
-		$required = ((string) $element['required'] == 'true' || (string) $element['required'] == 'required');
-		if (!$required && empty($value)) {
-			return true;
-		}
-
-		// Test the value against the regular expression.
-		if (!parent::test($element, $value, $group, $input, $form)) {
+		if(!parent::test($element, $value, $group, $input, $form)){
 			return false;
 		}
-
-		// Get params and component configurations
-		$app = JFactory::getApplication();
-		$params = new JRegistry;
-		$params->loadJSON($contact->params);
-		$pparams = $app->getParams('com_contact');
 		
-			// Determine banned emails
-		$configEmail	= $pparams->get('banned_email', '');
-		$paramsEmail	= $params->get('banned_mail', '');
-		$bannedEmail	= $configEmail . ($paramsEmail ? ';'.$paramsEmail : '');
+		$params = JComponentHelper::getParams('com_contact');
+		$banned = $params->get('banned_email');
+		
+		foreach(explode(';', $banned) as $item){
+			if (JString::stristr($item, $value) !== false)
+					return false;
+		}
+		
+		return true;
+	}
 
-		// Prevent form submission if one of the banned text is discovered in the email field
-		if (false === $this->_checkText($email, $bannedEmail)) {
-			$this->setError(JText::sprintf('COM_CONTACT_EMAIL_BANNEDTEXT', JText::_('JGLOBAL_EMAIL')));
-			return false;
-		}
-		return true;
-	}
-	
-	/**
-	 * Checks $text for values contained in the array $array, and sets error message if true...
-	 *
-	 * @param String	$text		Text to search against
-	 * @param String	$list		semicolon (;) seperated list of banned values
-	 * @return Boolean
-	 * @access protected
-	 * @since 1.5.4
-	 */
-	private function _checkText($text, $list) {
-		if (empty($list) || empty($text)) return true;
-		$array = explode(';', $list);
-		foreach ($array as $value) {
-			$value = trim($value);
-			if (empty($value)) continue;
-			if (JString::stristr($text, $value) !== false) {
-				return false;
-			}
-		}
-		return true;
-	}
 }
