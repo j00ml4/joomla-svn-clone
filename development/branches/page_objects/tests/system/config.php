ce7f828560e427ee6a8564441f1a6a25e21825c9
ian <ian@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-02-26 17:50:05 +0000

    Fleshing out page objects approach to testing

--- development/branches/page_objects/tests/system/config.php
+++ development/branches/page_objects/tests/system/config.php
@@ -1,3 +1,4 @@
 <?php
-$site = array('baseurl' => 'http://127.0.0.1:8080/trunk');
+
+$site = array('baseurl' => 'http://127.0.0.1/trunk');
 $selenium = array('browser' => '*chrome', 'host' => '127.0.0.1', 'port' => 4444);
