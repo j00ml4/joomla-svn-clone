ff1ea785a3419d8ca8035d8b51d3cccb6a202d67
mcsmom <mcsmom@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-03-09 21:45:41 +0000

    Update language strings and improve layout.

--- development/branches/contact/components/com_contact/views/contact/tmpl/default_form.php
+++ development/branches/contact/components/com_contact/views/contact/tmpl/default_form.php
@@ -19,31 +19,21 @@ JHtml::_('behavior.formvalidation');
 
 <div class="contact-form">
 	<form id="contact-form" action="<?php echo JRoute::_('index.php'); ?>" method="post" class="form-validate">
-		<?php foreach ($this->form->getFieldsets() as $fieldset): ?>
-			<?php $fields = $this->form->getFieldset($fieldset->name);?>
-			<?php if (count($fields)):?>
 				<fieldset>
-				<?php if (isset($fieldset->label)):?>
-					<legend><?php echo JText::_($fieldset->label);?></legend>
-				<?php endif;?>
-					<dl>
-				<?php foreach($fields as $field): ?>
-					<?php if ($field->hidden): ?>
-						<?php echo $field->input;?>
-					<?php else:?>
-						<dt>
-						<?php echo $field->label; ?>
-						<?php if (!$field->required && (!$field->type == "spacer")): ?>
-							<span class="optional"><?php echo JText::_('COM_CONTACT_OPTIONAL');?></span>
-						<?php endif; ?>
-						</dt>
-						<dd><?php echo $field->input;?></dd>
-					<?php endif;?>
-				<?php endforeach;?>
-					</dl>
-				</fieldset>
-			<?php endif;?>
-		<?php endforeach;?>
+		<legend><?php echo JText::_('COM_CONTACT_FORM_LABEL'); ?></legend>
+			<dl>
+				<dt><?php echo $this->form->getLabel('contact_name'); ?></dt>
+				<dd><?php echo $this->form->getInput('contact_name'); ?></dd>
+				<dt><?php echo $this->form->getLabel('contact_email'); ?></dt>
+				<dd><?php echo $this->form->getInput('contact_email'); ?></dd>
+				<dt><?php echo $this->form->getLabel('contact_subject'); ?></dt>
+				<dd><?php echo $this->form->getInput('contact_subject'); ?></dd>
+				<dt><?php echo $this->form->getLabel('contact_message'); ?></dt>
+				<dd><?php echo $this->form->getInput('contact_message'); ?></dd>
+				<dt><?php echo $this->form->getLabel('contact_email_copy'); ?></dt>
+				<dd><?php echo $this->form->getInput('contact_email_copy'); ?></dd>
+
+			</dl>
 		<div>
 			<button class="button validate" type="submit"><?php echo JText::_('COM_CONTACT_CONTACT_SEND'); ?></button>
 			<input type="hidden" name="option" value="com_contact" />
