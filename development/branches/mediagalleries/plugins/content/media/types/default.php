9387f4207c6dfff95a27fc8bdb4c4bb76b427e8b
3dentech <3dentech@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-03-19 18:18:01 +0000

    commit

--- development/branches/mediagalleries/plugins/content/media/types/default.php
+++
@@ -1,7 +0,0 @@
-<?php
-class MediaTypedefault extends MediaType{
-	public function getMedia($media='', $width='', $height='', $params=array()){
-    	
-    	return "<a href='$media' target='_blank'>$media</a>";
-    }
-}
--- development/branches/mediagalleries/plugins/content/media/types/mp4.php
+++ development/branches/mediagalleries/plugins/content/media/types/mp4.php
@@ -1,35 +1,28 @@
 <?php
-class MediaTypemp4 extends MediaType{
-	
-	public function getMedia($media='', $width='', $height='', $params=array()){
-    	$plugin = JPluginHelper::getPlugin('content', 'media');
-		$params = new JParameter($plugin->params);
-		$plgParams=$params;
-		$registry = new JRegistry();
-		$registry->loadJSON($params);
-		$params= $registry->toArray();
-		$plgParams= $registry->toObject();
-		$autostart=$plgParams->autostart;
-		
-    	$replace = '<object style="'.$width . $height.'" 
+
+class MediaTypemp4 extends MediaType {
+
+    public function getMedia($media='', $width='', $height='', $params=array()) {
+   
+        $autostart = $params->autostart;
+        $replace = '<object style="' . $width . $height . '"
 		classid="clsid:67DABFBF-D0AB-41fa-9C46-CC0F21721616"
 		codebase="http://go.divx.com/plugin/DivXBrowserPlugin.cab">'
-		. '<param name="src" value="'. $media .'" />'
-		. '<param name="custommode" value="none" />'
-		. '<param name="autoPlay" value="'.( ($autostart)? 'true': 'false' ).'" />'
-		. '<param name="mode" value="mini">'
-		. '<param name="allowContextMenu" value="true">'
-		. '<param name="bannerEnabled" value="false">'
-		. '<param name="bufferingMode" value="auto">'
-
-		. '<embed class="mediagalleries" style="'.$width . $height.'" src="'. $media .'" type="video/divx" 
-			custommode="none" autoPlay="'.( ($autostart)? 'true': 'false' ).'"  mode="mini"
+                . '<param name="src" value="' . $media . '" />'
+                . '<param name="custommode" value="none" />'
+                . '<param name="autoPlay" value="' . ( ($autostart) ? 'true' : 'false' ) . '" />'
+                . '<param name="mode" value="mini">'
+                . '<param name="allowContextMenu" value="true">'
+                . '<param name="bannerEnabled" value="false">'
+                . '<param name="bufferingMode" value="auto">'
+                . '<embed class="mediagalleries" style="' . $width . $height . '" src="' . $media . '" type="video/divx"
+			custommode="none" autoPlay="' . ( ($autostart) ? 'true' : 'false' ) . '"  mode="mini"
 			allowContextMenu="true" bannerEnabled="false" bufferingMode="auto"
 			pluginspage="http://go.divx.com/plugin/download/" ></embed>'
-	. '</object>';
+                . '</object>';
 
 
-	return $replace;
+        return $replace;
     }
 
 }
