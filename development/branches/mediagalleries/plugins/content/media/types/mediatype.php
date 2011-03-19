de5f039f579fafcc9e834f5bd533119c1b01110d
3dentech <3dentech@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-03-19 17:10:33 +0000

    merge

--- development/branches/mediagalleries/plugins/content/media/types/mediatype.php
+++ development/branches/mediagalleries/plugins/content/media/types/mediatype.php
@@ -17,7 +17,28 @@ abstract class MediaType{
     	
     	return 'Invalid Server';
     }
-    
+
+    public function extension( $video,$params ){
+	return 	'<object '. $params['a'] .' class="mediagalleries" style="'.$params['width'] . $params['height'].'" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">'
+		. '<param name="movie" value="'. $video .'" />'
+		. '<param name="wmode" value="transparent" />'
+		. $params['p'] // Params
+		. '<!--[if !IE]>-->'
+			. '<object '. $params['a'] .' class="mediagalleries" style="'.$params['width'] . $params['height'].'"
+				data="' .$video. '" type="application/x-shockwave-flash"> '
+				. '<param name="wmode" value="transparent" />'
+				. $params['p'] // Params
+		. '<!--<![endif]-->'
+				. '<a href="http://www.adobe.com/go/getflashplayer">
+						<img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif"
+							alt="Get Adobe Flash player" style="border:none;" /></a>'
+		. '<!--[if !IE]>-->'
+			. '</object>'
+		. '<!--<![endif]-->'
+	.'</object>';
+    }
+
+
     /**
      * 
      * Enter description here ...
