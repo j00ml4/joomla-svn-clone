160f1020dbf2f64b3baf21c258bb88bbb9c02b4b
dextercowley <dextercowley@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-03-31 15:58:17 +0000

    [#23053] 1.5.21 admin template params.ini error

--- development/releases/1.5/CHANGELOG.php
+++ development/releases/1.5/CHANGELOG.php
@@ -39,6 +39,7 @@ $ -> Language fix or change
 31-Mar-2011 Mark Dexter
  # [#22520] Articles archive years are HARDCODED!!!
  # [#25162] *SQL installation issue with TYPE
+ # [#23053] 1.5.21 admin template params.ini error
 
 
 -------------------- 1.5.22 Stable Release [03-November-2010] ------------------
