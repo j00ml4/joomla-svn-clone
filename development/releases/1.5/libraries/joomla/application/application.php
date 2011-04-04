09636ebfb9c11757d4608e7f8550f0dfb9a61a39
dextercowley <dextercowley@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-04-04 16:51:40 +0000

    Fix install bug from 23746 (revised)

--- development/releases/1.5/libraries/joomla/application/application.php
+++ development/releases/1.5/libraries/joomla/application/application.php
@@ -15,6 +15,8 @@
 // Check to ensure this file is within the rest of the framework
 defined('JPATH_BASE') or die();
 
+jimport('joomla.event.dispatcher');
+
 /**
 * Base class for a Joomla! application.
 *
