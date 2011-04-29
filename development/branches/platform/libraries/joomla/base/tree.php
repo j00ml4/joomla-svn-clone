f5c7a56a45e8b8cc6f639bd82f01dacf640bbf92
louis <louis@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-04-29 03:44:46 +0000

    Adding in new platform.

---
+++ development/branches/platform/libraries/joomla/base/tree.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Base
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+jimport('joomla.base.node');
+
+/**
+ * Tree Class.
+ *
+ * @package		Joomla.Platform
+ * @subpackage	Base
+ * @since		11.1
+ */
+class JTree extends JObject
+{
+	/**
+	 * Root node
+	 */
+	protected $_root = null;
+
+	/**
+	 * Current working node
+	 */
+	protected $_current = null;
+
+	function __construct()
+	{
+		$this->_root = new JNode('ROOT');
+		$this->_current = & $this->_root;
+	}
+
+	function addChild(&$node, $setCurrent = false)
+	{
+		$this->_current->addChild($node);
+		if ($setCurrent) {
+			$this->_current = &$node;
+		}
+	}
+
+	function getParent()
+	{
+		$this->_current = &$this->_current->getParent();
+	}
+
+	function reset()
+	{
+		$this->_current = &$this->_root;
+	}
+}
+
---
+++ development/branches/platform/libraries/joomla/document/renderer.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Document
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Abstract class for a renderer
+ *
+ * @abstract
+ * @package		Joomla.Platform
+ * @subpackage	Document
+ * @since		11.1
+ */
+class JDocumentRenderer extends JObject
+{
+	/**
+	* Reference to the JDocument object that instantiated the renderer
+	*
+	* @var		object
+	*/
+	protected	$_doc = null;
+
+	/**
+	 * Renderer mime type
+	 *
+	 * @var		string
+	 */
+	private $_mime = "text/html";
+
+	/**
+	* Class constructor
+	*
+	* @param object A reference to the JDocument object that instantiated the renderer
+	*/
+	public function __construct(&$doc)
+	{
+		$this->_doc = &$doc;
+	}
+
+	/**
+	 * Renders a script and returns the results as a string
+	 *
+	 * @param string	$name		The name of the element to render
+	 * @param array		$array		Array of values
+	 * @param string	$content	Override the output of the renderer
+	 *
+	 * @return string	The output of the script
+	 */
+	public function render($name, $params = null, $content = null)
+	{
+	}
+
+	/**
+	 * Return the content type of the renderer
+	 *
+	 * @return string The contentType
+	 */
+	function getContentType() {
+		return $this->_mime;
+	}
+}
