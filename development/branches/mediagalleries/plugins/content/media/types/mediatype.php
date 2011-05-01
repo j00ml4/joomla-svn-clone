732d82226c60aa3224e5e6fba984a2dc8c7ccebd
3dentech <3dentech@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-05-01 14:12:38 +0000

    now the youtube is working

--- development/branches/mediagalleries/plugins/content/media/types/mediatype.php
+++ development/branches/mediagalleries/plugins/content/media/types/mediatype.php
@@ -1,17 +1,17 @@
 <?php
-/* 
+
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
-class MediaType{
-    
+class MediaType {
+
     /**
      * @var String 
      */
     protected $media;
     protected $thumb;
-    
     /**
      * @var Array genearated by JParams
      */
@@ -22,20 +22,27 @@ class MediaType{
      * @param String $media Media Url
      * @param Array $params
      */
-    public function MediaType($media, $params){
-        $this->media  = $media;
-        $this->thumb  = $media;
-        $this->params = $params;
+    public function MediaType($media, $params) {
+        $this->media = $media;
+        $this->thumb = $media;
+        $this->params = &$params;
+
+        // Fix size
+        $params['width'] = 'width:' . $params['width'] . 'px;';
+        $params['height'] = 'height:' . $params['height'] . 'px;';
+
+        $params['thumb_width'] = 'width:' . $params['thumb_width'] . 'px;';
+        $params['thumb_height'] = 'height:' . $params['thumb_height'] . 'px;';
     }
-    
+
     /**
      * get the code
      * Enter description here ...
      * @param $media
      */
-    public function getMedia(){
-    	
-    	return 'Invalid Media: ' . $this->media;
+    public function getMedia() {
+
+        return 'Invalid Media: ' . $this->media;
     }
 
     /**
@@ -46,9 +53,9 @@ class MediaType{
      * @param unknown_type $height
      * @param unknown_type $params
      */
-    public function getThumb(){
-    	return "<img src='".$this->thumb."'".
-                    "style='".$this->params['thumb_width']." ".$this->params['thumb_height']."' >";
+    public function getThumb() {
+        return '<img src="' . $this->thumb . '"' .
+        'style="' . $this->params['thumb_width'] . $this->params['thumb_height'] . '" />';
     }
 
     /**
@@ -57,23 +64,24 @@ class MediaType{
      * @param <type> $params
      * @return <type>
      */
-    protected function html4Player($video, $params){
-	return 	'<object '. $params['autostart'] .' class="mediagalleries" style="'.$params['width'] . $params['height'].'" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">'
-		. '<param name="movie" value="'. $video .'" />'
-		. '<param name="wmode" value="transparent" />'
-		//. $params['p'] // Params
-		. '<!--[if !IE]>-->'
-			. '<object '. $params['autostart'] .' class="mediagalleries" style="'.$params['width'] . $params['height'].'"
-				data="' .$video. '" type="application/x-shockwave-flash"> '
-				. '<param name="wmode" value="transparent" />'
-				//. $params['p'] // Params
-		. '<!--<![endif]-->'
-				. '<a href="http://www.adobe.com/go/getflashplayer">
-						<img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif"
-							alt="Get Adobe Flash player" style="border:none;" /></a>'
-		. '<!--[if !IE]>-->'
-			. '</object>'
-		. '<!--<![endif]-->'
-	.'</object>';
-    }    
+    protected function html4Player($video, $params) {
+        return '<object ' . $params['autostart'] . ' class="mediagalleries" style="' . $params['width'] . $params['height'] . '" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">'
+        . '<param name="movie" value="' . $video . '" />'
+        . '<param name="wmode" value="transparent" />'
+        //. $params['p'] // Params
+        . '<!--[if!IE]>-->'
+        . '<object ' . $params['autostart'] . ' class="mediagalleries" style="' . $params['width'] . $params['height'] . '"
+        data="' . $video . '" type="application/x-shockwave-flash"> '
+        . '<param name="wmode" value="transparent" />'
+        //. $params['p'] // Params
+        . '<!--<![endif]-->'
+        . '<a href="http://www.adobe.com/go/getflashplayer">
+        <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif"
+        alt="Get Adobe Flash player" style="border:none;" /></a>'
+        . '<!--[if!IE]>-->'
+        . '</object>'
+        . '<!--<![endif]-->'
+        . '</object>';
+    }
+
 }
