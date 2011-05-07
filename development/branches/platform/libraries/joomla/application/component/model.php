3cd279bbfbd9453137bc4cfdd0dc35626cb75a07
dextercowley <dextercowley@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-05-07 16:38:16 +0000

    Fix bad method in JModel

--- development/branches/platform/libraries/joomla/application/component/model.php
+++ development/branches/platform/libraries/joomla/application/component/model.php
@@ -410,7 +410,9 @@ abstract class JModel extends JObject
 	 * @return    void
 	 * @since     11.1
 	 */
-	protected abstract function populateState();
+	protected function populateState()
+	{
+	}
 
 	/**
 	 * Method to set the database connector object
