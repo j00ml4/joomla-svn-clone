57659f8d940616364600c80256cac7bd03518612
realityking <realityking@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-05-24 21:00:15 +0000

    Let's get rid of some more markup in the views :)

--- development/branches/installation/installation/views/language/tmpl/default.php
+++ development/branches/installation/installation/views/language/tmpl/default.php
@@ -8,39 +8,34 @@
 
 defined('_JEXEC') or die;
 ?>
-<form action="index.php" method="post" id="adminForm" class="form-validate">
-	<div id="right">
-		<div id="rightpad">
-			<div id="step">
-				<div class="far-right">
-				<?php if ($this->document->direction == 'ltr') : ?>
-					<div class="button1-left"><div class="next"><a href="#" onclick="Install.submitform('setup.setlanguage');" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a></div></div>
-				<?php elseif ($this->document->direction == 'rtl') : ?>
-					<div class="button1-right"><div class="prev"><a href="#" onclick="Install.submitform('setup.setlanguage');" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a></div></div>
-				<?php endif; ?>
-				</div>
-				<span class="steptitle"><?php echo JText::_('INSTL_LANGUAGE_TITLE'); ?></span>
-			</div>
-			<div id="installer">
-
-				<div class="m">
-					<h2><?php echo JText::_('INSTL_SELECT_LANGUAGE_TITLE'); ?></h2>
-					<div class="install-text">
-						<?php echo JText::_('INSTL_SELECT_LANGUAGE_DESC'); ?>
-					</div>
-					<div class="install-body">
-						<div class="m">
-							<fieldset>
-								<?php echo $this->form->getInput('language'); ?>
-							</fieldset>
-						</div>
-						<div class="clr"></div>
-					</div>
-					<div class="clr"></div>
-				</div>
-			</div>
-		</div>
-		<input type="hidden" name="task" value="" />
-		<?php echo JHtml::_('form.token'); ?>
+<div id="step">
+	<div class="far-right">
+	<?php if ($this->document->direction == 'ltr') : ?>
+		<div class="button1-left"><div class="next"><a href="#" onclick="Install.submitform('setup.setlanguage');" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a></div></div>
+	<?php elseif ($this->document->direction == 'rtl') : ?>
+		<div class="button1-right"><div class="prev"><a href="#" onclick="Install.submitform('setup.setlanguage');" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a></div></div>
+	<?php endif; ?>
 	</div>
+	<span class="steptitle"><?php echo JText::_('INSTL_LANGUAGE_TITLE'); ?></span>
+</div>
+<form action="index.php" method="post" id="adminForm" class="form-validate">
+	<div id="installer">
+		<div class="m">
+			<h2><?php echo JText::_('INSTL_SELECT_LANGUAGE_TITLE'); ?></h2>
+			<div class="install-text">
+				<?php echo JText::_('INSTL_SELECT_LANGUAGE_DESC'); ?>
+			</div>
+			<div class="install-body">
+				<div class="m">
+					<fieldset>
+						<?php echo $this->form->getInput('language'); ?>
+					</fieldset>
+				</div>
+				<div class="clr"></div>
+			</div>
+			<div class="clr"></div>
+		</div>
+	</div>
+	<input type="hidden" name="task" value="" />
+	<?php echo JHtml::_('form.token'); ?>
 </form>
