530ea548936aeda0ff67b95c95177bd32ba4face
rcorral <rcorral@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-06-18 00:14:06 +0000

    Updating to latest trunk version

--- development/branches/rcorral/components/com_contact/views/contact/tmpl/default_form.php
+++ development/branches/rcorral/components/com_contact/views/contact/tmpl/default_form.php
@@ -34,11 +34,31 @@ JHtml::_('behavior.tooltip');
 						<dt><?php echo $this->form->getLabel('contact_email_copy'); ?></dt>
 						<dd><?php echo $this->form->getInput('contact_email_copy'); ?></dd>
 				<?php 	} ?>
+			<?php //Dynamically load any additional fields from plugins. ?>
+			     <?php foreach ($this->form->getFieldsets() as $fieldset): ?>
+			          <?php if ($fieldset->name != 'contact'):?>
+			               <?php $fields = $this->form->getFieldset($fieldset->name);?>
+			               <?php foreach($fields as $field): ?>
+			                    <?php if ($field->hidden): ?>
+			                         <?php echo $field->input;?>
+			                    <?php else:?>
+			                         <dt>
+			                            <?php echo $field->label; ?>
+			                            <?php if (!$field->required && $field->type != "Spacer"): ?>
+			                               <span class="optional"><?php echo JText::_('COM_CONTACT_OPTIONAL');?></span>
+			                            <?php endif; ?>
+			                         </dt>
+			                         <dd><?php echo $field->input;?></dd>
+			                    <?php endif;?>
+			               <?php endforeach;?>
+			          <?php endif ?>
+			     <?php endforeach;?>
 				<dt></dt>
 				<dd><button class="button validate" type="submit"><?php echo JText::_('COM_CONTACT_CONTACT_SEND'); ?></button>
 					<input type="hidden" name="option" value="com_contact" />
 					<input type="hidden" name="task" value="contact.submit" />
 					<input type="hidden" name="return" value="<?php echo $this->return_page;?>" />
+					<input type="hidden" name="id" value="<?php echo $this->contact->slug; ?>" />
 					<?php echo JHtml::_( 'form.token' ); ?>
 				</dd>
 			</dl>
