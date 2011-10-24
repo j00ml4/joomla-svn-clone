e3b3e024d4ede6d3238a3c51aa22f4a05b0819c1
hooduku <hooduku@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-10-24 12:24:47 +0000

    Lock and unlock implemented

--- development/branches/db17/libraries/joomla/database/database/sqlazurequery.php
+++ development/branches/db17/libraries/joomla/database/database/sqlazurequery.php
@@ -188,4 +188,26 @@ class JDatabaseQuerySQLAzure extends JDatabaseQuery
 	{
 		return 'LEN('.$value.')';
 	}
+	
+	/**
+	 * Method to lock the database table for writing.
+	 *
+	 * @return boolean True on success.
+	 * @since 11.1
+	 */
+	public function lock($table_name, &$db)
+	{
+		 return true;
+	}
+
+	/**
+	 * Method to unlock the database table for writing.
+	 *
+	 * @return boolean True on success.
+	 * @since 11.1
+	 */
+	public function unlock(&$db)
+	{
+		 return true;
+	}
 }
