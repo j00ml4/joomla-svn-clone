c97b2cde0946b289923ee3ee5085e4d26c8ea8d9
hooduku <hooduku@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-11-02 06:58:12 +0000

    Fixed casewhen  - concat and casttoChar is replaced by concatenate and castAsChar.

--- development/branches/db17/plugins/content/pagenavigation/pagenavigation.php
+++ development/branches/db17/plugins/content/pagenavigation/pagenavigation.php
@@ -104,22 +104,22 @@ class plgContentPagenavigation extends JPlugin
 
 			// Array of articles in same category correctly ordered.
 			$query	= $db->getQuery(true);
-      //sqlsrv changes
-      $case_when = ' CASE WHEN ';
-      $case_when .= $query->charLength('a.alias');
-      $case_when .= ' THEN ';
-      $a_id = $query->castToChar('a.id');
-      $case_when .= $query->concat(array($a_id, 'a.alias'), ':');
-      $case_when .= ' ELSE ';
-      $case_when .= $a_id.' END as slug';   
-            
-      $case_when1 = ' CASE WHEN ';
-      $case_when1 .= $query->charLength('cc.alias');
-      $case_when1 .= ' THEN ';
-      $c_id = $query->castToChar('cc.id');
-      $case_when1 .= $query->concat(array($c_id, 'cc.alias'), ':');
-      $case_when1 .= ' ELSE ';
-      $case_when1 .= $c_id.' END as catslug'; 
+	       //sqlsrv changes
+	        $case_when = ' CASE WHEN ';
+	        $case_when .= $query->charLength('a.alias');
+	        $case_when .= ' THEN ';
+	        $a_id = $query->castAsChar('a.id');
+	        $case_when .= $query->concatenate(array($a_id, 'a.alias'), ':');
+	        $case_when .= ' ELSE ';
+	        $case_when .= $a_id.' END as slug';   
+	            
+	        $case_when1 = ' CASE WHEN ';
+	        $case_when1 .= $query->charLength('cc.alias');
+	        $case_when1 .= ' THEN ';
+	        $c_id = $query->castAsChar('cc.id');
+	        $case_when1 .= $query->concatenate(array($c_id, 'cc.alias'), ':');
+	        $case_when1 .= ' ELSE ';
+	        $case_when1 .= $c_id.' END as catslug'; 
       
 			$query->select('a.id,'.$case_when.','.$case_when1);
 			$query->from('#__content AS a');
