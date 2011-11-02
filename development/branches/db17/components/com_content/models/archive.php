41fba4a0d9f19071b479ccd243875c46f27dbe80
hooduku <hooduku@6f6e1ebd-4c2b-0410-823f-f34bde69bce9>
2011-11-02 06:27:56 +0000

    Fixed casewhen  - concat and casttoChar is replaced by concatenate and castAsChar.

--- development/branches/db17/components/com_content/models/archive.php
+++ development/branches/db17/components/com_content/models/archive.php
@@ -85,8 +85,8 @@ class ContentModelArchive extends ContentModelArticles
 		$case_when = ' CASE WHEN ';
     $case_when .= $query->charLength('a.alias');
     $case_when .= ' THEN ';
-    $a_id = $query->castToChar('a.id');
-    $case_when .= $query->concat(array($a_id, 'a.alias'), ':');
+    $a_id = $query->castAsChar('a.id');
+    $case_when .= $query->concatenate(array($a_id, 'a.alias'), ':');
     $case_when .= ' ELSE ';
     $case_when .= $a_id.' END as slug';   
 		//$query->select(' CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(\':\', a.id, a.alias) ELSE a.id END as slug');
@@ -95,8 +95,8 @@ class ContentModelArchive extends ContentModelArticles
     $case_when = ' CASE WHEN ';
     $case_when .= $query->charLength('c.alias');
     $case_when .= ' THEN ';
-    $c_id = $query->castToChar('c.id');
-    $case_when .= $query->concat(array($c_id, 'c.alias'), ':');
+    $c_id = $query->castAsChar('c.id');
+    $case_when .= $query->concatenate(array($c_id, 'c.alias'), ':');
     $case_when .= ' ELSE ';
     $case_when .= $c_id.' END as catslug'; 
     $query->select($case_when); 
@@ -150,6 +150,7 @@ class ContentModelArchive extends ContentModelArticles
 	// JModel override to add alternating value for $odd
 	protected function _getList($query, $limitstart=0, $limit=0)
 	{
+		echo $query;
 		$result = &parent::_getList($query, $limitstart, $limit);
 
 		$odd = 1;
